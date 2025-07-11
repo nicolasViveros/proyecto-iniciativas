24ca77060c219b4b196b3eb1ee118a318b4617f9
Rubén Alvarado <ruben.alvarado@amaris.comm>
2025-07-11 12:34:42 -0400

    cors

--- src/app.js
+++ src/app.js
@@ -1,56 +1,55 @@
-import express  from 'express'
-import morgan from 'morgan'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
-import multer from 'multer'
+import express from "express";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import multer from "multer";
 
+import authRoutes from "./routes/auth.routes.js";
+import tasksRoutes from "./routes/tasks.routes.js";
+import fichasRoutes from "./routes/fichas.routes.js";
 
-import authRoutes from './routes/auth.routes.js'
-import tasksRoutes from './routes/tasks.routes.js'
-import fichasRoutes from './routes/fichas.routes.js'
+const app = express();
 
-
-const app = express()
-
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials:true  
-}));
-app.use(morgan('dev'));
-//app.use(express.json()); 
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(morgan("dev"));
+//app.use(express.json());
 app.use(cookieParser());
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-app.use("/api",authRoutes);  
-app.use("/api",tasksRoutes);
-app.use("/api",fichasRoutes);
+app.use("/api", authRoutes);
+app.use("/api", tasksRoutes);
+app.use("/api", fichasRoutes);
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "uploads/"); // crea la carpeta si no existe
-    },
-    filename: function (req, file, cb) {
-      cb(null, Date.now() + "-" + file.originalname);
-    },
-  });
+  destination: function (req, file, cb) {
+    cb(null, "uploads/"); // crea la carpeta si no existe
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
 
-  const upload = multer({ storage });
+const upload = multer({ storage });
 
-  
-  app.post('/uploads', upload.single('file'), (req, res) => {
-    try {
-      if (!req.file) {
-        return res.status(400).send('No file uploaded.');
-      }
-      // Aquí podrías procesar el archivo, guardarlo, etc.
-      res.send('File uploaded successfully.');
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Error processing the file');
+app.post("/uploads", upload.single("file"), (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).send("No file uploaded.");
     }
-  });
+    // Aquí podrías procesar el archivo, guardarlo, etc.
+    res.send("File uploaded successfully.");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error processing the file");
+  }
+});
 
-  
-export default app 
+export default app;
