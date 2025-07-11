e00d7bcebe00163ebff952a06d4d288976ab43df
Rubén Alvarado <ruben.alvarado@amaris.comm>
2025-07-11 12:48:13 -0400

    cors

--- client3/vite.config.js
+++ client3/vite.config.js
@@ -1,24 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcsspost from '@tailwindcss/postcss'
-import tailwindcss from '@tailwindcss/vite'
-
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcsspost from "@tailwindcss/postcss";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   css: {
     postcss: {
       plugins: [tailwindcsspost()],
     },
-  }
-})
+  },
+  server: {
+    host: "0.0.0.0", // para aceptar conexiones externas
+    port: 80, // si sigues usando el puerto 80
+    allowedHosts: ["rumboalaequidad.org"],
+  },
+});
 
 // // https://vite.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
 // })
-
 
 // import { defineConfig } from 'vite'
 // import tailwindcss from '@tailwindcss/vite'
--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
+    //credentials: true,
   })
 );
 app.use(morgan("dev"));
