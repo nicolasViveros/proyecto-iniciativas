27e232efded85b0b59f56f803bcaa16dea587b02
Rubén Alvarado <ruben.alvarado@amaris.comm>
2025-07-11 12:53:02 -0400

    cors

--- client3/src/api/axios.js
+++ client3/src/api/axios.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-    baseURL:'http://62.171.132.171:4000/api',
-    withCredentials: true
-})
+  baseURL: "http://62.171.132.171:4000/api",
+  withCredentials: false,
+});
 
-export default instance
+export default instance;
--- client3/vite.config.js
+++ client3/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     },
   },
   server: {
-    host: "0.0.0.0",
+    host: "*",
     port: 80,
     allowedHosts: ["rumboalaequidad.org", "www.rumboalaequidad.org"], // Added www.rumboalaequidad.org
   },
