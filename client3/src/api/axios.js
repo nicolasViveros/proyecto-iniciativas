1c1567cff551666ee50cba90294127ca9add4a2e
Rubén Alvarado <ruben.alvarado@amaris.comm>
2025-07-12 19:34:38 -0400

    fix https back

--- client3/src/api/axios.js
+++ client3/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://rumboalaequidad.org/api",
+  baseURL: "https://rumboalaequidad.org:4000/api",
   withCredentials: true,
 });
 
