273e694504c4d50fd4fd657f52e986420f1847d4
Rubén Alvarado <ruben.alvarado@amaris.comm>
2025-07-12 19:29:43 -0400

    fix https back

--- client3/src/api/axios.js
+++ client3/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://62.171.132.171:4000/api",
-    withCredentials: true,
-  });
+  baseURL: "https://rumboalaequidad.org/api",
+  withCredentials: true,
+});
 
 export default instance;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-import app from './app.js'
-import { conectDB } from './db.js'
+import app from "./app.js";
+import { conectDB } from "./db.js";
+import fs from "fs";
+
+const privateKey = fs.readFileSync("/path/to/mi_clave_privada.key", "utf8");
+const certificate = fs.readFileSync("/path/to/certificado.crt", "utf8");
+
+const credentials = { key: privateKey, cert: certificate };
+const httpsServer = https.createServer(credentials, app);
 
 conectDB();
-app.listen(4000)
-console.log('server on port' ,4000)
-
+httpsServer.listen(4000);
+console.log("server on port", 4000);
