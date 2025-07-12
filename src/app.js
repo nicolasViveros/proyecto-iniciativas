ecde182764d7c1e024185f3518320e826571a601
Rubén Alvarado <ruben.alvarado@amaris.comm>
2025-07-12 19:36:03 -0400

    fix https back

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://rumboalaequidad.org",
+    origin: "https://rumboalaequidad.org",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true, // Esto debe ser verdadero para permitir cookies
   })
