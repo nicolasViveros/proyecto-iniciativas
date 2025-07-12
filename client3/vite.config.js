b5b16f7141f1c1e2406922f36c96eed24ef76c45
Rubén Alvarado <ruben.alvarado@amaris.comm>
2025-07-11 21:27:04 -0400

    install ssl basic

--- client3/vite.config.js
+++ client3/vite.config.js
@@ -8,11 +8,11 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    basicSsl({
-      name: "mi_solicitud.csr",
-      domains: ["*.rumboalaequidad.org"],
-      //certDir: "/root/proyecto-iniciativas",
-    }),
+    // basicSsl({
+    //   name: "mi_solicitud.csr",
+    //   domains: ["*.rumboalaequidad.org"],
+    //   //certDir: "/root/proyecto-iniciativas",
+    // }),
   ],
   css: {
     postcss: {
@@ -24,7 +24,7 @@ export default defineConfig({
     port: 443,
     allowedHosts: ["rumboalaequidad.org", "www.rumboalaequidad.org"],
     https: true,
-    key: "/../../../mi_clave_privada.key", // Reemplaza con la ruta a tu clave privada
-    cert: "../../../mi_solicitud.csr",
+    key: "/../../../../mi_clave_privada.key", // Reemplaza con la ruta a tu clave privada
+    cert: "../../../../mi_solicitud.csr",
   },
 });
