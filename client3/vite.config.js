264a97c6ed355915dd7e136894b4672abe1eade5
Rubén Alvarado <ruben.alvarado@amaris.comm>
2025-07-11 21:41:54 -0400

    install ssl basic

--- client3/vite.config.js
+++ client3/vite.config.js
@@ -2,18 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcsspost from "@tailwindcss/postcss";
 import tailwindcss from "@tailwindcss/vite";
-import basicSsl from "@vitejs/plugin-basic-ssl";
+//import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    basicSsl({
-      name: "tu_certificado.crt",
-      domains: ["*.rumboalaequidad.org", "*.www.rumboalaequidad.org"],
-      keyDir: "/root/proyecto-iniciativas",
-      certDir: "/root/proyecto-iniciativas",
-    }),
+    // basicSsl({
+    //   name: "tu_certificado.crt",
+    //   domains: ["*.rumboalaequidad.org", "*.www.rumboalaequidad.org"],
+    //   keyDir: "/root/proyecto-iniciativas",
+    //   certDir: "/root/proyecto-iniciativas",
+    // }),
   ],
   css: {
     postcss: {
@@ -24,10 +24,10 @@ export default defineConfig({
     host: "*",
     port: 443,
     allowedHosts: ["rumboalaequidad.org", "www.rumboalaequidad.org"],
-    https: true,
-    // https: {
-    //   key: "/root/proyecto-iniciativas/mi_clave_privada.key", // Path to your private key
-    //   cert: "/root/proyecto-iniciativas/tu_certificado.crt", // Path to your issued SSL certificate
-    // },
+    //https: true,
+    https: {
+      key: "/root/proyecto-iniciativas/mi_clave_privada.key", // Path to your private key
+      cert: "/root/proyecto-iniciativas/tu_certificado.crt", // Path to your issued SSL certificate
+    },
   },
 });
