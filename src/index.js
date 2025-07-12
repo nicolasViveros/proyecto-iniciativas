7c60b8050d40e2f01fa3e0dfadaec3699232eaf6
Rubén Alvarado <ruben.alvarado@amaris.comm>
2025-07-12 19:31:11 -0400

    fix https back

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import app from "./app.js";
 import { conectDB } from "./db.js";
 import fs from "fs";
 
-const privateKey = fs.readFileSync("/path/to/mi_clave_privada.key", "utf8");
-const certificate = fs.readFileSync("/path/to/certificado.crt", "utf8");
+const privateKey = fs.readFileSync(
+  "/root/proyecto-iniciativas/mi_clave_privada.key",
+  "utf8"
+);
+const certificate = fs.readFileSync(
+  "/root/proyecto-iniciativas/certificado.crt",
+  "utf8"
+);
 
 const credentials = { key: privateKey, cert: certificate };
 const httpsServer = https.createServer(credentials, app);
