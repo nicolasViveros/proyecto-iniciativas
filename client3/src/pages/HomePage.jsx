87060b2362ba0fd0a031fbd4dd782a4061684a4c
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-14 20:37:11 -0400

    se arregla flujo de botones

--- client3/src/pages/HomePage.jsx
+++ client3/src/pages/HomePage.jsx
@@ -25,9 +25,9 @@ function HomePage() {
     {
       titulo: "Categoría de postulación",
       contenido: `El concurso contempla dos categorías:
-      Instituciones públicas y Empresas privadas:
+      **Instituciones públicas y Empresas privadas:**
       Orientada a instituciones públicas, privadas y/o sociedades de capital mixto responsables de la regulación del transporte urbano, así como a operadores de transporte, que hayan implementado iniciativas concretas para superar brechas de género en la movilidad urbana. Se valorarán acciones que promuevan la igualdad de oportunidades, la participación de las mujeres en el sector, la mejora de condiciones laborales con enfoque de género, o el diseño e implementación de servicios más seguros, accesibles e inclusivos.
-      Organizaciones de la sociedad civil y Entidades académicas
+      **Organizaciones de la sociedad civil y Entidades académicas**
       Dirigida a organizaciones de la sociedad civil y entidades académicas que hayan desarrollado investigaciones, proyectos piloto, campañas, metodologías o programas de formación que promuevan la equidad de género en el ámbito de la movilidad urbana. Se priorizarán aquellas propuestas que hayan generado aprendizajes valiosos, incidencia en políticas públicas, articulación intersectorial o que tengan potencial de escalabilidad y réplica en otros contextos urbanos.`,
     },
     {
