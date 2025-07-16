bad6e421cb4e2ceef12f2e52ad39e136f81ea1bb
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-15 20:27:27 -0400

    15 jul - crud de fichas

--- client3/src/pages/FichasPage.jsx
+++ client3/src/pages/FichasPage.jsx
@@ -53,8 +53,8 @@ function FichasPage() {
     <div className="container mx-auto p-6">
       <h1 className="text-3xl font-bold mb-6">Listado de Fichas</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {fichas.map((ficha, index) => (
-          <FichaCard key={index} ficha={ficha} />
+        {fichas.map((ficha) => (
+          <FichaCard ficha={ficha} key={ficha._id}  />
         ))}
       </div>
     </div>
