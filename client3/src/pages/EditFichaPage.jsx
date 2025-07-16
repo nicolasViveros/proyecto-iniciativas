c46b0ac495eeb432e845cf829cd72fdbe6d5b942
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-16 11:32:42 -0400

    15 jul - crud de fichas

--- client3/src/pages/EditFichaPage.jsx
+++ client3/src/pages/EditFichaPage.jsx
@@ -80,7 +80,7 @@ function EditFichaPage() {
     return (
         <div >
             <h1 className="text-3xl text-center font-bold mb-4">{ficha.name}</h1>
-            <div className="container grid grid-cols-2 gap-4 mx-auto p-6">
+            <div className="container grid grid-cols-2 mx-auto p-2">
                 <div>Organización:</div>
                 <div><input
                     type="text"
