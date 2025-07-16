356a2598aff08a5c0e044c2b024722bb37ba84f1
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-15 20:36:57 -0400

    15 jul - crud de fichas

--- client3/src/pages/FichasPage.jsx
+++ client3/src/pages/FichasPage.jsx
@@ -1,6 +1,4 @@
 import { useEffect } from 'react'
-import { useParams } from 'react-router-dom';
-
 import { useFichas } from '../context/FichasContext'
 import { Link } from 'react-router-dom'; // Asegúrate de tener react-router-dom instalado
 
@@ -9,7 +7,7 @@ function FichaCard({ ficha }) {
   return (
     <div className="border p-4 rounded mb-4 shadow">
       <h2 className="font-bold text-lg">
-        <Link to={`/ficha/${ficha._id}`} className="text-blue-500 hover:underline flex justify-center">
+        <Link to={`/ficha/${ficha._id}`} className="text-xl hover:underline flex justify-center">
           Proyecto: {ficha.name}
         </Link>
       </h2>
@@ -19,11 +17,11 @@ function FichaCard({ ficha }) {
       <p><strong>Email:</strong> {ficha.email}</p>
       <p><strong>Teléfono:</strong> {ficha.phone}</p>
       <p><strong>Asociaciones:</strong> {ficha.associations.join(', ')}</p>
-      <>
+      
+      <div className="flex gap-x-2 place-content-end-safe ">
         <button
           className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md"
           onClick={() => {
-            console.log(ficha._id)
             deleteFicha(ficha._id);
           }}
         >
@@ -35,7 +33,7 @@ function FichaCard({ ficha }) {
         >
           editar
         </Link>
-      </>
+      </div>
     </div>
   );
 }
@@ -54,7 +52,7 @@ function FichasPage() {
       <h1 className="text-3xl font-bold mb-6">Listado de Fichas</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {fichas.map((ficha) => (
-          <FichaCard ficha={ficha} key={ficha._id}  />
+          <FichaCard ficha={ficha} key={ficha._id} />
         ))}
       </div>
     </div>
