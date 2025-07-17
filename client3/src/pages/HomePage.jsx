e00ab67d6e19fbe58b114a56f5b3c0c078811d2a
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-17 19:35:32 -0400

    17 jul banner

--- client3/src/pages/HomePage.jsx
+++ client3/src/pages/HomePage.jsx
@@ -1372,15 +1372,15 @@ function HomePage() {
           className="w-full h-auto object-cover"
         />
         <div className="absolute top-3/8 left-2/12 transform -traslate-x-3/12">
-          <p className="text-2xl sm:text-xl font-medium text-[#5A478D]">concurso</p>
-          <div className="bg-[#e79953] text-white inline-block px-2 py-1 text-3xl md:text-sm font-bold rounded-sm mb-2">
+          <p className="text-2xl font-medium text-[#5A478D]">concurso</p>
+          <div className="bg-[#e79953] text-white inline-block px-2 py-1 text-3xl  font-bold rounded-sm mb-2">
             ¡Postula tu Iniciativa!
           </div>
 
-          <h2 className="text-2xl md:text-lg lg:text-xl font-extrabold text-[#3C3270] mb-1">
+          <h2 className="text-2xl  font-extrabold text-[#3C3270] mb-1">
             RUMBO A LA EQUIDAD:
           </h2>
-          <p className="text-xl md:text-sm lg:text-base text-[#5A478D] leading-tight">
+          <p className="text-xl  text-[#5A478D] leading-tight">
             Reconociendo iniciativas innovadoras en favor de la <br />
             equidad de género en movilidad urbana
           </p>
