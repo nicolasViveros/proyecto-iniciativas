eb1807d1ef54692847d9b61cbb56433e9105af66
Rubén Alvarado <serveralvarado@gmail.com>
2025-07-17 13:13:22 -0400

    Update email.controller.js

--- src/controllers/email.controller.js
+++ src/controllers/email.controller.js
@@ -10,7 +10,7 @@ export const sendEmail = async ({
 }) => {
   try {
     let transporter = nodemailer.createTransport({
-      host: 'rumboalaequidad.org',
+      host: 'localhost',
       port: 25,
       secure: false, // For port 25, usually false, but STARTTLS might be attempted
       // If your local SMTP server requires authentication, uncomment and fill this:
