b6c647771dee7e4c081910d0d82d29da92a38d14
Rubén Alvarado <ruben.alvarado@amaris.comm>
2025-07-17 09:58:11 -0400

    fix save ficha

--- src/controllers/fichas.controller.js
+++ src/controllers/fichas.controller.js
@@ -1,7 +1,5 @@
-import Ficha from "../models/ficha.model.js";
-import { sendEmail } from "../controllers/email.controller.js";
-
 export const createFicha = async (req, res) => {
+  let savedFicha;
   try {
     const {
       organizationName,
@@ -73,12 +71,21 @@ export const createFicha = async (req, res) => {
       accepted,
     });
 
-    const savedFicha = await newFicha.save();
-    await sendEmail({
-      recipientEmail: email,
-      recipientName: name,
-      emailSubject: "Gracias por participar. Rumbo a la Equidad",
-      htmlContent: `
+    savedFicha = await newFicha.save();
+  } catch (error) {
+    console.error("Error during the save operation:", error);
+    return res
+      .status(500)
+      .json({ message: "Algo salió mal", error: error.message });
+  }
+
+  // Separate email sending logic
+
+  await sendEmail({
+    recipientEmail: email,
+    recipientName: name,
+    emailSubject: "Gracias por participar. Rumbo a la Equidad",
+    htmlContent: `
         <div style="color: #5d5593">
           <h1>Su postulación ha sido guardada</h1>
           <p>Gracias por participar en Rumbo a la Equidad. Aquí están los detalles de su participación:</p>
@@ -96,57 +103,10 @@ export const createFicha = async (req, res) => {
         </div>
         <img src='https://rumboalaequidad.org/Footer2.png' alt='logo' style="display: block; margin-top: 10px;">
       `,
-      textContent:
-        "Su postulación ha sido guardada. Gracias por participar en Rumbo a la Equidad.",
-    });
-    res.json(savedFicha);
-  } catch (error) {
-    // Proporciona más detalles sobre el error
-    console.error("Error during the save operation:", error);
-    return res
-      .status(500)
-      .json({ message: "Algo salió mal", error: error.message });
-  }
-};
+    textContent:
+      "Su postulación ha sido guardada. Gracias por participar en Rumbo a la Equidad.",
+  });
 
-export const getFichas = async (req, res) => {
-  try {
-    const fichas = await Ficha.find().select("-files -acceptanceLetter");
-    res.json(fichas);
-  } catch (error) {
-    return res.status(500).json({ message: "algo va mal" });
-  }
-};
-
-export const getFicha = async (req, res) => {
-  try {
-    const ficha = await Ficha.findById(req.params.id);
-    if (!ficha) return res.status(404).json({ message: "ficha not found" });
-    res.json(ficha);
-  } catch (error) {
-    return res.status(404).json({ message: "ficha not found" });
-  }
-};
-
-export const deleteFicha = async (req, res) => {
-  try {
-    console.log(req.params.id);
-    const ficha = await Ficha.findByIdAndDelete(req.params.id);
-    if (!ficha) return res.status(404).json({ message: "ficha not found" });
-    return res.sendStatus(204);
-  } catch (error) {
-    return res.status(404).json({ message: "ficha not found" });
-  }
-};
-
-export const updateFicha = async (req, res) => {
-  try {
-    const ficha = await Ficha.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    });
-    if (!ficha) return res.status(404).json({ message: "ficha not found" });
-    res.json(ficha);
-  } catch (error) {
-    return res.status(404).json({ message: "ficha not found" });
-  }
+  // Respond with the saved Ficha
+  return res.json(savedFicha);
 };
