6e614120e88c26ab7fe642ed2e2f898b8f1b918a
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-17 20:52:08 -0400

    17 jul banner

--- client3/src/pages/HomePage.jsx
+++ client3/src/pages/HomePage.jsx
@@ -1376,7 +1376,7 @@ function HomePage() {
           alt="Banner mobile"
           className="w-full h-auto object-cover block sm:hidden"
         />
-        <div className="absolute top-2/8 left-2/12 transform -traslate-x-3/12 sm:top1/12 sm:left-2">
+        <div className="absolute sm:top-2/8 sm:left-2/12 transform -traslate-x-3/12 top1/12 left-2">
           <p className="text-2xl md:text-xl sm:text-base font-medium text-[#5A478D]">concurso</p>
           <div className="bg-[#e79953] text-3xl md:text-2xl sm:text-lg text-white inline-block px-2 py-1  font-bold rounded-sm mb-2">
             ¡Postula tu Iniciativa!
