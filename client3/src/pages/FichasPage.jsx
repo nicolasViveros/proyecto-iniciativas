d137b2c37b435904e09f4cd00d75e4b362d7739b
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-18 11:56:49 -0400

    17 jul banner

--- client3/src/pages/FichasPage.jsx
+++ client3/src/pages/FichasPage.jsx
@@ -18,7 +18,7 @@ function FichaCard({ ficha }) {
       <p><strong>Teléfono:</strong> {ficha.phone}</p>
       <p><strong>Asociaciones:</strong> {ficha.associations.join(', ')}</p>
       
-      <div className="absolute bottom-4 right-2 gap-x-2 place-content-end-safe ">
+      <div className="flex bottom-0 gap-x-2 place-content-end-safe ">
         <button
           className="bg-[#5d5593] text-white px-4 py-2 rounded hover:bg-[#a49fc4]"
           onClick={() => {
