f1460ee500691385eb64828bba3cb698470624a5
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-18 11:28:50 -0400

    17 jul banner

--- client3/src/pages/FormStepByStep.jsx
+++ client3/src/pages/FormStepByStep.jsx
@@ -1523,7 +1523,7 @@ export default function FormWizard() {
                 type="submit"
                 className={`text-white px-4 py-2 rounded ${form.accepted
                   ? "bg-[#5d5593] hover:bg-[#a49fc4]"
-                  : "bg-purple-300"
+                  : "bg-silver-300"
                   }`}
                 disabled={!form.accepted || isSubmitted}
               >
