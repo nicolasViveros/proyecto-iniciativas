3200d5feaca953e50d83af25cff5eaa6d8c9cae1
Rubén Alvarado <ruben.alvarado@amaris.comm>
2025-07-18 13:31:01 -0400

    disable overlay

--- client3/vite.config.js
+++ client3/vite.config.js
@@ -29,5 +29,8 @@ export default defineConfig({
       key: "/root/proyecto-iniciativas/mi_clave_privada.key", // Path to your private key
       cert: "/root/proyecto-iniciativas/certificado.crt", // Path to your issued SSL certificate
     },
+    hmr: {
+      overlay: false, // Disable error overlay
+    },
   },
 });
