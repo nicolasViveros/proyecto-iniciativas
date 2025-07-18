c3e18d621b3fa41c7cef62aed552ef2bc864d2a5
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-18 11:04:41 -0400

    17 jul banner

--- client3/src/pages/FormStepByStep.jsx
+++ client3/src/pages/FormStepByStep.jsx
@@ -59,7 +59,7 @@ function Step1({
   return (
 
     <div>
-      
+
       <h1 className="text-3xl text-center font-bold mb-4">
         {translateText[language].organizationDetails}
       </h1>
@@ -1089,17 +1089,15 @@ export default function FormWizard() {
         if (!member.email) {
           newErrors.push(
             language === "es"
-              ? `El correo electrónico del miembro ${
-                  index + 1
-                } del equipo es requerido.`
+              ? `El correo electrónico del miembro ${index + 1
+              } del equipo es requerido.`
               : `Team member ${index + 1} email is required.`
           );
         } else if (!isValidEmail(member.email)) {
           newErrors.push(
             language === "es"
-              ? `El correo electrónico del miembro ${
-                  index + 1
-                } tiene un formato inválido.`
+              ? `El correo electrónico del miembro ${index + 1
+              } tiene un formato inválido.`
               : `Team member ${index + 1} email has an invalid format.`
           );
         }
@@ -1424,7 +1422,7 @@ export default function FormWizard() {
     />,
   ];
 
-   const switchToSpanish = () => {
+  const switchToSpanish = () => {
     if (language !== "es") toggleLanguage();
   };
 
@@ -1435,8 +1433,7 @@ export default function FormWizard() {
 
   return (
     <div className="relative flex items-center justify-center min-h-screen">
-      
-      
+
       <button
         onClick={handleBack}
         className="absolute top-4 left-4 hover:text-[#a49fc4] rounded-md"
@@ -1448,6 +1445,11 @@ export default function FormWizard() {
             : translateText[language].previous}
         </span>
       </button>
+
+      <div className="absolute top-4 right-4 space-x-2">
+        <span className="fi fi-es cursor-pointer" onClick={switchToSpanish}></span>
+        <span className="fi fi-gb cursor-pointer" onClick={switchToEnglish}></span>
+      </div>
 
       <div className="max-w-3xl w-full p-10 rounded-md">
         <div className="mb-6">
@@ -1528,11 +1530,10 @@ export default function FormWizard() {
               </button>
               <button
                 type="submit"
-                className={`text-white px-4 py-2 rounded ${
-                  form.accepted
-                    ? "bg-[#5d5593] hover:bg-[#a49fc4]"
-                    : "bg-purple-300"
-                }`}
+                className={`text-white px-4 py-2 rounded ${form.accepted
+                  ? "bg-[#5d5593] hover:bg-[#a49fc4]"
+                  : "bg-purple-300"
+                  }`}
                 disabled={!form.accepted || isSubmitted}
               >
                 {translateText[language].submit}
