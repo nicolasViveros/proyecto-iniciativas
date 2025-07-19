25b45a60b6b6e647f5d473626c2e02e721adb648
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-19 00:28:45 -0400

    17 jul banner

--- client3/src/components/NavBar.jsx
+++ client3/src/components/NavBar.jsx
@@ -6,7 +6,7 @@ function NavBar() {
 
     return (
         <nav className=" flex justify-between border-b border-gray-300 ">
-            <footer className="bg-white w-fit">
+            <footer className="bg-white w-full">
                 <div className=" inset-0 flex items-center justify-between ">
                     {/* Logo izquierda (GIZ) */}
                     <img
