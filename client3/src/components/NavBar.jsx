db2434cac0fd8d1e809f05137e350dd88514970c
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-19 00:23:18 -0400

    17 jul banner

--- client3/src/components/NavBar.jsx
+++ client3/src/components/NavBar.jsx
@@ -12,7 +12,7 @@ function NavBar() {
                     <img
                         src="/global_gateway.png"
                         alt="GIZ Logo"
-                        className="h-35 md:h-45 lg:h-50"
+                        className="h-35 md:h-45 lg:h-30"
                     />
 
                     {/* Logo derecha (Somos LAC) */}
