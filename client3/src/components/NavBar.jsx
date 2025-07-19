d2dc5a0c3259ef930abeba7409a93a7030cc82c2
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-18 23:33:04 -0400

    17 jul banner

--- client3/src/components/NavBar.jsx
+++ client3/src/components/NavBar.jsx
@@ -5,15 +5,16 @@ function NavBar() {
 
 
     return (
-        <nav className="my-3 flex justify-between border-b border-gray-300 px-4 md:px-10">
+        <nav className="my-3 border-b border-gray-300">
             <footer className="bg-white w-full">
-                <div className="px-4 py-3 flex items-center justify-between">
-                    <div className="object-contain flex items-center space-x-4 md:space-x-12">
-                        <Link to="/">
-                            <img src="/Header1.svg" alt="Global Gateway" className="h-16 md:h-25" />
-                        </Link>
-                    </div>
-                   
+                <div className="flex justify-between items-center px-4 md:px-10">
+                    <Link to="/" className="flex-grow">
+                        <img
+                            src="/Header1.svg"
+                            alt="Global Gateway"
+                            className="w-full object-cover"
+                        />
+                    </Link>
                 </div>
             </footer>
         </nav>
