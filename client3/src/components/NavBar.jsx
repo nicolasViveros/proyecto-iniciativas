d565861b4aa4830e325d469a3cfd92a8e0811171
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-18 23:48:54 -0400

    17 jul banner

--- client3/src/components/NavBar.jsx
+++ client3/src/components/NavBar.jsx
@@ -5,67 +5,19 @@ function NavBar() {
 
 
     return (
-        <nav className="my-3 border-b border-gray-300">
+        <nav className="my-3 flex justify-between border-b border-gray-300 px-4 md:px-10">
             <footer className="bg-white w-full">
-                <div className="flex justify-between object-contain  items-center my-4 md:px-10 h-16 md:h-25">
-                    <Link to="/" className="flex-grow">
-                        <img
-                            src="/Header_1.png"
-                            alt="Global Gateway"
-                            className="w-full inset-0 h-50 object-contain"
-                        />
-                    </Link>
+                <div className="px-4 py-3 flex items-center justify-between">
+                    <div className="flex items-center space-x-4 md:space-x-12">
+                        <Link to="/">
+                            <img src="/Header_1.png" alt="Global Gateway" className="h-16 md:h-25" />
+                        </Link>
+                    </div>
+                   
                 </div>
             </footer>
         </nav>
     );
 }
-
-{/* <Link to={
-                isAuthenticated ? "/tasks" : "/"
-            }>
-                <h1 className="text-4xl font-bold">Iniciativas GIZ</h1>
-            </Link>
-            <ul className="flex gap-x-2">
-                {isAuthenticated ? (
-                    <>
-                        <li>
-                            Bienvenido {user.username}
-                        </li>
-                        <li>
-                            <Link to='/add-task' className="bg-indigo-500 hover:bg-indigo-600 px-4 py-2 rounded-sm">Añadir iniciativa</Link>
-                        </li>
-                        <li>
-                            <Link to='/new-task' className="bg-indigo-500 hover:bg-indigo-600 px-4 py-2 rounded-sm">nueva iniciativa</Link>
-                        </li>
-                        <li>
-                            <Link to='/nueva-ficha' className="bg-indigo-500 hover:bg-indigo-600 px-4 py-2 rounded-sm">paso a paso</Link>
-                        </li>
-                        <li>
-                            <Link to='/' onClick={() => {
-                                logout();
-                            }}
-                                className="hover:text-red-400">
-                                Cerrar sesión
-                            </Link>
-                        </li>
-                    </>
-                ) : (
-                    <>
-                        <li>
-                            <Link to='/nueva-ficha' className="bg-indigo-500 hover:bg-indigo-600 px-4 py-2 rounded-sm">Nueva ficha</Link>
-                        </li>
-                        <li>
-                            <Link to='/login' className="bg-indigo-500 px-4 py-1 rounded-sm">Accede</Link>
-                        </li>
-                        <li>
-                            <Link to='/register' className="bg-indigo-500 px-4 py-1 rounded-sm">Registrate</Link>
-                        </li>
-                    </>
-
-
-                )}
-            </ul> */}
-
 
 export default NavBar
