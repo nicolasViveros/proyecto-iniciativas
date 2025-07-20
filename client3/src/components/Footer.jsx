e7166b2f2b220d56863e80416e49d0945e41a6f2
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-20 08:29:51 -0400

    17 jul banner

--- client3/src/components/Footer.jsx
+++ client3/src/components/Footer.jsx
@@ -4,10 +4,11 @@ function Footer() {
   return (
     <footer className="relative w-full flex flex-col md:flex-row h-auto">
       <div>
-        <span className="text-[#5D5593] text-2xs justify-start">Implementado por:</span>
 
         {/* Logo izquierda (GIZ) con fondo blanco */}
         <div className="flex bg-white w-full md:w-auto px-4 items-center justify-center md:justify-start h-16 md:h-auto">
+        <span className="text-[#5D5593] text-2xs justify-start">Implementado por:</span>
+
           <img
             src="/LogoGiz.svg"
             alt="GIZ Logo"
