a70cfeaa2a726890d220d16133ec04572fd82232
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-20 19:26:32 -0400

    20 jul

--- client3/src/pages/FichasPage.jsx
+++ client3/src/pages/FichasPage.jsx
@@ -18,7 +18,7 @@ function FichaCard({ ficha }) {
       <p><strong>Teléfono:</strong> {ficha.phone}</p>
       <p><strong>Asociaciones:</strong> {ficha.associations.join(', ')}</p>
       
-      <div className=" flex gap-x-2 place-items-end-safe justify-end">
+      <div className=" flex gap-x-2 mt-auto items-end justify-end">
       <button
           className="bg-[#5d5593] text-white px-4 py-2 rounded hover:bg-[#a49fc4]"
           onClick={() => {
