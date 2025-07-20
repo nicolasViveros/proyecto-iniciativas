9b28f64390dc2dbbc89f92736b2fb5d361f863b8
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-20 08:48:11 -0400

    20 jul

--- client3/src/components/Footer.jsx
+++ client3/src/components/Footer.jsx
@@ -2,25 +2,28 @@ import { Link } from "react-router-dom"
 import { useAuth } from "../context/AuthContext"
 function Footer() {
   return (
-    <footer className="relative w-full flex flex-col md:flex-row h-auto">
-      {/* Logo izquierda (GIZ) con fondo blanco */}
-      <div className="flex bg-white w-full md:w-auto px-4 items-center justify-center md:justify-start h-16 md:h-auto">
-        <img
-          src="/LogoGiz.svg"
-          alt="GIZ Logo"
-          className="h-12 md:h-16 lg:h-20"
-        />
-      </div>
+    <div>
+      <span className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-white to-transparent">Implementado por</span>
+      <footer className="relative w-full flex flex-col md:flex-row h-auto">
+        {/* Logo izquierda (GIZ) con fondo blanco */}
+        <div className="flex bg-white w-full md:w-auto px-4 items-center justify-center md:justify-start h-16 md:h-auto">
+          <img
+            src="/LogoGiz.svg"
+            alt="GIZ Logo"
+            className="h-12 md:h-16 lg:h-20"
+          />
+        </div>
 
-      {/* Fondo restante con imagen de footer */}
-      <div className="flex-grow relative overflow-hidden h-32 md:h-auto">
-        <img
-          src="/Footer2.png"
-          alt="Footer"
-          className="absolute inset-0 w-full h-full object-cover"
-        />
-      </div>
-    </footer>
+        {/* Fondo restante con imagen de footer */}
+        <div className="flex-grow relative overflow-hidden h-32 md:h-auto">
+          <img
+            src="/Footer2.png"
+            alt="Footer"
+            className="absolute inset-0 w-full h-full object-cover"
+          />
+        </div>
+      </footer>
+    </div>
   );
 }
 
