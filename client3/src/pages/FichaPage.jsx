3cbf82480e566043720c5df2fa08f52ab9d3aaea
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-20 18:32:54 -0400

    20 jul

--- client3/src/pages/FichaPage.jsx
+++ client3/src/pages/FichaPage.jsx
@@ -32,7 +32,7 @@ const FichaPage = () => {
 
   if (!ficha) {
     return <div className="relative flex items-center justify-center min-h-screen">
-      <div className="max-w-3xl w-full p-10 rounded-md">
+      <div className="max-w-3xl w-full p-10 rounded-md justify-center items-center">
         Cargando ficha...</div>
     </div>;
   }
