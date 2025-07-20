8c17660ba5b98a5678cee722e480129a4a541719
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-20 18:17:22 -0400

    20 jul

--- client3/src/pages/FichaPage.jsx
+++ client3/src/pages/FichaPage.jsx
@@ -93,7 +93,7 @@ const FichaPage = () => {
             <p className="col-span-2 border-b border-[#D9D6E1] pb-1">{ficha.need}</p>
 
           <p><strong>Objetivos del Proyecto:</strong> </p>
-          <p className="border-b border-[#D9D6E1] pb-1">{ficha.objectives}</p>
+          <p className=" col-span-2 border-b border-[#D9D6E1] pb-1">{ficha.objectives}</p>
 
           <p><strong>Público Objetivo:</strong> </p>
           <p className="col-span-2 border-b border-[#D9D6E1] pb-1">{ficha.targetAudience}</p>
@@ -123,7 +123,7 @@ const FichaPage = () => {
           <p className="col-span-2 border-b border-[#D9D6E1] pb-1">{ficha.transferability}</p>
 
           <p><strong>Sostenibilidad:</strong> </p>
-          <p >{ficha.sustainability}</p>
+          <p className='col-span-2 '>{ficha.sustainability}</p>
 
           <p className='col-span-2 underline mb-3 className="col-span-2 border-t border-[#D9D6E1] pb-1"'><strong>Material de Respaldo:</strong></p>
           <ul>
