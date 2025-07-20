63553e8f9a3c14c81190e299da5811342111e84f
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-20 09:03:42 -0400

    20 jul

--- client3/src/components/Footer.jsx
+++ client3/src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div>
       <div>
-              <span className=" px-1/2 sm:text-xs text-xl ">Implementado por:</span>
+              <span className=" pl-4 sm:text-xs text-xl ">Implementado por:</span>
       </div>
       <footer className="relative w-full flex flex-col md:flex-row h-auto">
         {/* Logo izquierda (GIZ) con fondo blanco */}
