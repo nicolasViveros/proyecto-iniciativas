f818662fa51a6825f2c4e81f155e0cf6dfe3d4e2
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-20 08:23:23 -0400

    17 jul banner

--- client3/src/components/Footer.jsx
+++ client3/src/components/Footer.jsx
@@ -3,9 +3,10 @@ import { useAuth } from "../context/AuthContext"
 function Footer() {
   return (
     <footer className="relative w-full flex flex-col md:flex-row h-auto">
-      <span className="text-[#5D5593] justify-start">Implementado por:</span>
       {/* Logo izquierda (GIZ) con fondo blanco */}
-      <div className="flex bg-white w-full md:w-auto px-4 items-center justify-center md:justify-start h-16 md:h-auto">
+      <div className="flex grid-rows-2 bg-white w-full md:w-auto px-4 items-center justify-center md:justify-start h-16 md:h-auto">
+      <span className="text-[#5D5593] justify-start">Implementado por:</span>
+
         <img
           src="/LogoGiz.svg"
           alt="GIZ Logo"
