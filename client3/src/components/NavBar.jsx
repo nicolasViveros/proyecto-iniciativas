96f51e3748c372546d457a6c1529a88aa922e123
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-20 18:56:23 -0400

    20 jul

--- client3/src/components/NavBar.jsx
+++ client3/src/components/NavBar.jsx
@@ -26,14 +26,18 @@ function NavBar() {
                             alt="Somos LAC Logo"
                             className="mb-6 h-25 md:h-35 lg:h-45"
                         />
-                        <li>
-                            <Link to='/' onClick={() => {
-                                logout();
-                            }}
-                                className="hover:text-red-400">
-                                Cerrar sesión
-                            </Link>
-                        </li>
+
+                        <Link to='/fichas'
+                            className="hover:text-red-400 absolute top-4 left-4 text-xs">
+                            Fichas
+                        </Link>
+                        <Link to='/' onClick={() => {
+                            logout();
+                        }}
+                            className="hover:text-red-400 absolute top-4 left-4 text-xs">
+                            Cerrar sesión
+                        </Link>
+
                     </div>
 
 
