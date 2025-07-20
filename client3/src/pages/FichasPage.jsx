ab7912c86dbb869b47163f9f5ab8b0766dc80e3e
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-20 19:35:07 -0400

    20 jul

--- client3/src/pages/FichasPage.jsx
+++ client3/src/pages/FichasPage.jsx
@@ -7,20 +7,21 @@ function FichaCard({ ficha }) {
   return (
     <div className="border p-4 rounded mb-4 shadow">
       <div >
-      <h2 className="font-bold text-lg">
-        <Link to={`/ficha/${ficha._id}`} className="text-xl hover:underline flex justify-center">
-          Proyecto: {ficha.name}
-        </Link>
-      </h2>
-      <p><strong>Tipo de Organización:</strong> {ficha.organizationType}</p>
-      <p><strong>País:</strong> {ficha.country}</p>
-      <p><strong>Representante Legal:</strong> {ficha.legalRepName}</p>
-      <p><strong>Email:</strong> {ficha.email}</p>
-      <p><strong>Teléfono:</strong> {ficha.phone}</p>
-      <p><strong>Asociaciones:</strong> {ficha.associations.join(', ')}</p>
+        <h2 className="font-bold text-lg">
+          <Link to={`/ficha/${ficha._id}`} className="text-xl hover:underline flex justify-center">
+            Proyecto: {ficha.name}
+          </Link>
+        </h2>
+        <p><strong>Tipo de Organización:</strong> {ficha.organizationType}</p>
+        <p><strong>País:</strong> {ficha.country}</p>
+        <p><strong>Representante Legal:</strong> {ficha.legalRepName}</p>
+        <p><strong>Email:</strong> {ficha.email}</p>
+        <p><strong>Teléfono:</strong> {ficha.phone}</p>
+        <p><strong>Asociaciones:</strong> {ficha.associations.join(', ')}</p>
       </div>
-      <div className=" flex flex-col gap-x-2 mt-auto items-end justify-end min-h-[250px]">
-      <button
+
+      <div className=" grid grid-cols-2 gap-x-2 mt-auto items-end justify-beetween space-beetween min-h-[250px]">
+        <button
           className="bg-[#5d5593] text-white px-4 py-2 rounded hover:bg-[#a49fc4]"
           onClick={() => {
             if (window.confirm("¿Está seguro de que desea eliminar esta ficha? Esta acción no se puede deshacer.")) {
@@ -48,11 +49,11 @@ function FichasPage() {
     getFichas();
   }, []);
 
-  if (fichas.length == 0) return ( <div className="  items-center justify-center min-h-screen">
-  <div className="max-w-3xl w-full rounded-md justify-center items-center">
-    Cargando fichas...
-  </div>
-</div>);
+  if (fichas.length == 0) return (<div className="  items-center justify-center min-h-screen">
+    <div className="max-w-3xl w-full rounded-md justify-center items-center">
+      Cargando fichas...
+    </div>
+  </div>);
 
   return (
     <div className="container mx-auto p-6">
