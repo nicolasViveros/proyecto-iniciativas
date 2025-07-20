4f294dfdf6100ec9722ebdd0febd528e96b4751a
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-20 18:23:31 -0400

    20 jul

--- client3/src/pages/FichaPage.jsx
+++ client3/src/pages/FichaPage.jsx
@@ -4,7 +4,7 @@ import React, { use, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useFichas } from '../context/FichasContext';
 import { FaArrowCircleLeft } from "react-icons/fa";
-
+import { Link } from 'react-router-dom';
 
 const FichaPage = () => {
   const { id } = useParams();
@@ -46,7 +46,13 @@ const FichaPage = () => {
         <FaArrowCircleLeft className="text-2xl mr-1 mb-1 inline" />
         <span className="ml-1">Volver</span>
       </button>
-
+      <div className="flex justify-end p-4 space-x-2">
+        <Link
+          to={`/ficha/${ficha._id}/editar`} className="bg-[#5d5593] text-white px-4 py-2 rounded hover:bg-[#a49fc4]"
+        >
+          editar
+        </Link>
+      </div>
       <div className="max-w-3xl w-full p-10 rounded-md">
 
         <h1 className="text-3xl text-center font-bold mb-4">{ficha.name}</h1>
@@ -89,8 +95,8 @@ const FichaPage = () => {
               </div>
             ))}
           </div>
-            <p><strong>Necesidad/Problemática:</strong> </p>
-            <p className="col-span-2 border-b border-[#D9D6E1] pb-1">{ficha.need}</p>
+          <p><strong>Necesidad/Problemática:</strong> </p>
+          <p className="col-span-2 border-b border-[#D9D6E1] pb-1">{ficha.need}</p>
 
           <p><strong>Objetivos del Proyecto:</strong> </p>
           <p className=" col-span-2 border-b border-[#D9D6E1] pb-1">{ficha.objectives}</p>
