64397026bed2d1a0fed12adb89724abb9c386e2b
Rubén Alvarado <ruben.alvarado@amaris.comm>
2025-07-21 17:51:21 -0400

    test files download

--- client3/src/pages/FichaPage.jsx
+++ client3/src/pages/FichaPage.jsx
@@ -1,11 +1,9 @@
-
-
-import React, { use, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { useFichas } from '../context/FichasContext';
+import React, { use, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useFichas } from "../context/FichasContext";
 import { FaArrowCircleLeft } from "react-icons/fa";
-import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const FichaPage = () => {
   const { id } = useParams();
@@ -21,9 +19,8 @@ const FichaPage = () => {
         console.log(ficha.projectName);
         setFicha(ficha);
         console.log(ficha);
-
       } catch (error) {
-        console.error('Error al cargar la ficha:', error);
+        console.error("Error al cargar la ficha:", error);
       }
     };
 
@@ -33,123 +30,231 @@ const FichaPage = () => {
   }, [id]);
 
   if (!ficha) {
-    return <div className="  items-center justify-center min-h-screen">
-      <div className="max-w-3xl w-full rounded-md justify-center items-center">
-        Cargando ficha...</div>
-    </div>;
+    return (
+      <div className="  items-center justify-center min-h-screen">
+        <div className="max-w-3xl w-full rounded-md justify-center items-center">
+          Cargando ficha...
+        </div>
+      </div>
+    );
   }
 
   const handleBack = () => {
-
-    navigate('/fichas'); // vuelve al listado de fichas
-
+    navigate("/fichas"); // vuelve al listado de fichas
   };
   return (
-    <div className="relative flex items-center justify-center min-h-screen" >
-
-      <button onClick={handleBack} className="absolute top-4 left-4 hover:text-[#a49fc4] rounded-md">
+    <div className="relative flex items-center justify-center min-h-screen">
+      <button
+        onClick={handleBack}
+        className="absolute top-4 left-4 hover:text-[#a49fc4] rounded-md"
+      >
         <FaArrowCircleLeft className="text-2xl mr-1 mb-1 inline" />
         <span className="ml-1">Volver</span>
       </button>
 
       <div className="absolute top-4 right-4 space-x-2">
         <Link
-          to={`/ficha/${ficha._id}/editar`} className="bg-[#5d5593] text-white px-4 py-2 rounded hover:bg-[#a49fc4]"
+          to={`/ficha/${ficha._id}/editar`}
+          className="bg-[#5d5593] text-white px-4 py-2 rounded hover:bg-[#a49fc4]"
         >
           editar
         </Link>
       </div>
 
       <div className="max-w-3xl w-full p-10 rounded-md">
-
         <h1 className="text-3xl text-center font-bold mb-4">{ficha.name}</h1>
         <div className="container grid grid-cols-2 mx-auto p-2">
-
-          <p><strong>Organización:</strong> </p>
+          <p>
+            <strong>Organización:</strong>{" "}
+          </p>
           <p>{ficha.organizationName}</p>
-          <p><strong>Tipo de Organización:</strong> </p>
+          <p>
+            <strong>Tipo de Organización:</strong>{" "}
+          </p>
           <p>{ficha.organizationType}</p>
-          <p><strong>País:</strong> </p>
+          <p>
+            <strong>País:</strong>{" "}
+          </p>
           <p>{ficha.country}</p>
-          <p><strong>Representante Legal:</strong> </p>
+          <p>
+            <strong>Representante Legal:</strong>{" "}
+          </p>
           <p>{ficha.legalRepName}</p>
-          <p><strong>Cargo del Representante Legal:</strong> </p>
+          <p>
+            <strong>Cargo del Representante Legal:</strong>{" "}
+          </p>
           <p>{ficha.legalRepPosition}</p>
-          <p><strong>Email:</strong> </p>
+          <p>
+            <strong>Email:</strong>{" "}
+          </p>
           <p>{ficha.email}</p>
-          <p><strong>Teléfono:</strong> </p>
+          <p>
+            <strong>Teléfono:</strong>{" "}
+          </p>
           <p>{ficha.phone}</p>
-          <p><strong>Registro Legal:</strong> </p>
+          <p>
+            <strong>Registro Legal:</strong>{" "}
+          </p>
           <p>{ficha.registrationId}</p>
-          <p><strong>Ciudad de Implementación:</strong> </p>
+          <p>
+            <strong>Ciudad de Implementación:</strong>{" "}
+          </p>
           <p>{ficha.city}</p>
-          <p><strong>Fecha de Inicio:</strong> </p>
+          <p>
+            <strong>Fecha de Inicio:</strong>{" "}
+          </p>
           <p>{new Date(ficha.startDate).toLocaleDateString()}</p>
-          <p><strong>¿Está Vigente?:</strong> </p>
+          <p>
+            <strong>¿Está Vigente?:</strong>{" "}
+          </p>
           <p>{ficha.isActive ? "Sí" : "No"}</p>
-          {!ficha.isActive && <p><strong>Razón Inactiva:</strong> {ficha.reasonInactive}</p>}
-          <p><strong>Asociaciones:</strong> </p>
-          <p>{ficha.associations.length > 0 ? ficha.associations.join(', ') : 'Ninguna'}</p>
-          <div className='col-span-2'>
-            <p className='border-b border-[#D9D6E1] pb-1'><strong>Equipo Responsable:</strong></p>
+          {!ficha.isActive && (
+            <p>
+              <strong>Razón Inactiva:</strong> {ficha.reasonInactive}
+            </p>
+          )}
+          <p>
+            <strong>Asociaciones:</strong>{" "}
+          </p>
+          <p>
+            {ficha.associations.length > 0
+              ? ficha.associations.join(", ")
+              : "Ninguna"}
+          </p>
+          <div className="col-span-2">
+            <p className="border-b border-[#D9D6E1] pb-1">
+              <strong>Equipo Responsable:</strong>
+            </p>
             {ficha.team.map((member, index) => (
               <div key={index}>
-                <p className='col-span-2'>Miembro {index + 1} del equipo:</p>
-                <p className='col-span-2'>nombre - {member.name}</p>
-                <p className='col-span-2'>cargo - {member.position}</p>
-                <p className='col-span-2 border-b border-[#D9D6E1] pb-1'>correo - {member.email}</p>
-
+                <p className="col-span-2">Miembro {index + 1} del equipo:</p>
+                <p className="col-span-2">nombre - {member.name}</p>
+                <p className="col-span-2">cargo - {member.position}</p>
+                <p className="col-span-2 border-b border-[#D9D6E1] pb-1">
+                  correo - {member.email}
+                </p>
               </div>
             ))}
           </div>
-          <p><strong>Necesidad/Problemática:</strong> </p>
-          <p className="col-span-2 border-b border-[#D9D6E1] pb-1">{ficha.need}</p>
+          <p>
+            <strong>Necesidad/Problemática:</strong>{" "}
+          </p>
+          <p className="col-span-2 border-b border-[#D9D6E1] pb-1">
+            {ficha.need}
+          </p>
 
-          <p><strong>Objetivos del Proyecto:</strong> </p>
-          <p className=" col-span-2 border-b border-[#D9D6E1] pb-1">{ficha.objectives}</p>
+          <p>
+            <strong>Objetivos del Proyecto:</strong>{" "}
+          </p>
+          <p className=" col-span-2 border-b border-[#D9D6E1] pb-1">
+            {ficha.objectives}
+          </p>
 
-          <p><strong>Público Objetivo:</strong> </p>
-          <p className="col-span-2 border-b border-[#D9D6E1] pb-1">{ficha.targetAudience}</p>
+          <p>
+            <strong>Público Objetivo:</strong>{" "}
+          </p>
+          <p className="col-span-2 border-b border-[#D9D6E1] pb-1">
+            {ficha.targetAudience}
+          </p>
 
-          <p><strong>Actividades Principales:</strong> </p>
-          <p className="col-span-2 border-b border-[#D9D6E1] pb-1">{ficha.activities}</p>
+          <p>
+            <strong>Actividades Principales:</strong>{" "}
+          </p>
+          <p className="col-span-2 border-b border-[#D9D6E1] pb-1">
+            {ficha.activities}
+          </p>
 
-          {ficha.category === 'Operador/Regulador' && (
+          {ficha.category === "Operador/Regulador" && (
             <>
-              <p><strong>Innovación:</strong> </p>
-              <p className="col-span-2 border-b border-[#D9D6E1] pb-1">{ficha.innovation}</p>
+              <p>
+                <strong>Innovación:</strong>{" "}
+              </p>
+              <p className="col-span-2 border-b border-[#D9D6E1] pb-1">
+                {ficha.innovation}
+              </p>
 
-              <p><strong>Impacto:</strong> </p>
-              <p className="col-span-2 border-b border-[#D9D6E1] pb-1">{ficha.impact}</p>
+              <p>
+                <strong>Impacto:</strong>{" "}
+              </p>
+              <p className="col-span-2 border-b border-[#D9D6E1] pb-1">
+                {ficha.impact}
+              </p>
             </>
           )}
-          {ficha.category === 'ONG/Academia' && (
+          {ficha.category === "ONG/Academia" && (
             <>
-              <p><strong>Metodología:</strong> </p>
-              <p className="col-span-2 border-b border-[#D9D6E1] pb-1">{ficha.methodology}</p>
+              <p>
+                <strong>Metodología:</strong>{" "}
+              </p>
+              <p className="col-span-2 border-b border-[#D9D6E1] pb-1">
+                {ficha.methodology}
+              </p>
 
-              <p><strong>Resultados:</strong> </p>
-              <p className="col-span-2 border-b border-[#D9D6E1] pb-1">{ficha.outcomes}</p>
+              <p>
+                <strong>Resultados:</strong>{" "}
+              </p>
+              <p className="col-span-2 border-b border-[#D9D6E1] pb-1">
+                {ficha.outcomes}
+              </p>
             </>
           )}
-          <p><strong>Transferibilidad:</strong> </p>
-          <p className="col-span-2 border-b border-[#D9D6E1] pb-1">{ficha.transferability}</p>
+          <p>
+            <strong>Transferibilidad:</strong>{" "}
+          </p>
+          <p className="col-span-2 border-b border-[#D9D6E1] pb-1">
+            {ficha.transferability}
+          </p>
 
-          <p><strong>Sostenibilidad:</strong> </p>
-          <p className='col-span-2 pb-1 '>{ficha.sustainability}</p>
+          <p>
+            <strong>Sostenibilidad:</strong>{" "}
+          </p>
+          <p className="col-span-2 pb-1 ">{ficha.sustainability}</p>
 
-          <p className='col-span-2 mb-3 className="col-span-2 border-t border-[#D9D6E1] pb-1"'><strong>Material de Respaldo:</strong></p>
+          <p className='col-span-2 mb-3 className="col-span-2 border-t border-[#D9D6E1] pb-1"'>
+            <strong>Material de Respaldo:</strong>
+          </p>
           <ul>
-
             {ficha.links.map((link, index) => (
-              <p key={index} className='col-span-2'><strong>Link:</strong><a href={link} target="_blank" rel="noopener noreferrer">{link}</a></p>
+              <p key={index} className="col-span-2">
+                <strong>Link:</strong>
+                <a href={link} target="_blank" rel="noopener noreferrer">
+                  {link}
+                </a>
+              </p>
             ))}
           </ul>
           {ficha.video && (
-            <p className='col-span-2'><strong>Video:</strong> <a href={ficha.video} target="_blank" rel="noopener noreferrer">{ficha.video}</a></p>
+            <p className="col-span-2">
+              <strong>Video:</strong>{" "}
+              <a href={ficha.video} target="_blank" rel="noopener noreferrer">
+                {ficha.video}
+              </a>
+            </p>
           )}
           {ficha.recognition && (
-            <p className='col-span-2'><strong>Reconocimientos:</strong> {ficha.recognition}</p>
+            <p className="col-span-2">
+              <strong>Reconocimientos:</strong> {ficha.recognition}
+            </p>
+          )}
+          {ficha.files && (
+            <div>
+              <p className="col-span-2">
+                <strong>Archivos:</strong>
+              </p>
+              <ul>
+                {ficha.files.map((file, index) => (
+                  <li key={index} className="col-span-2">
+                    <strong>Archivo:</strong>{" "}
+                    <a
+                      href={`data:${file.type};base64,${file.content}`}
+                      download={file.name}
+                    >
+                      {file.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
           )}
         </div>
       </div>
