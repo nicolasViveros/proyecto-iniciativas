cf661166ba92f15d1aa35fd262b10970d8c32507
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-22 03:13:38 -0400

    22 jul

--- client3/src/pages/FichaPage.jsx
+++ client3/src/pages/FichaPage.jsx
@@ -257,7 +257,7 @@ const FichaPage = () => {
           {ficha.video && (
             <p className="col-span-2">
               <strong>Video:</strong>{" "}
-              <a className="w-screen" href={ficha.video} target="_blank" rel="noopener noreferrer">
+              <a className="w-fit" href={ficha.video} target="_blank" rel="noopener noreferrer">
                 {ficha.video}
               </a>
             </p>
