ac521b8dcddd85125c7bf0fbe5a0789d8745b71f
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-22 18:19:54 -0400

    22 julio

--- client3/src/components/Footer.jsx
+++ client3/src/components/Footer.jsx
@@ -16,7 +16,7 @@ function Footer() {
           </div>
 
           {/* Logo izquierda (GIZ) con fondo blanco */}
-          <div className=" bg-white w-full md:w-auto px-4 items-start justify-start md:justify-start h-fit">
+          <div className=" bg-white w-full md:w-auto px-4 items-start justify-start md:justify-start h-auto">
             <span className="flex text-xs  ">Implementado por:</span>
 
             <Link to="/">
