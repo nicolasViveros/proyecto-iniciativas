9800d32b8bca19624d6e47351ee9fcf649624e82
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-22 14:20:21 -0400

    22 julio

--- client3/src/context/FichasContext.jsx
+++ client3/src/context/FichasContext.jsx
@@ -1,94 +1,129 @@
 import { createContext, useContext, useState } from "react";
 import {
-  createFichaRequest,
-  getFichasRequest,
-  getFichaRequest,
-  deleteFichaRequest,
-  updateFichaRequest,
+    createFichaRequest,
+    getFichasRequest,
+    getFichaRequest,
+    deleteFichaRequest,
+    updateFichaRequest,
 } from "../api/fichas";
 
 const FichaContext = createContext();
 
 export const useFichas = () => {
-  const context = useContext(FichaContext);
+    const context = useContext(FichaContext);
 
-  if (!context) {
-    throw new Error("useFichas must be used within a FichaProvider");
-  }
-  return context;
+    if (!context) {
+        throw new Error("useFichas must be used within a FichaProvider");
+    }
+    return context;
 };
 
 export function FichaProvider({ children }) {
-  const [fichas, setFichas] = useState([]);
+    const [fichas, setFichas] = useState([]);
 
-  const getFichas = async () => {
-    try {
-      const res = await getFichasRequest();
-      setFichas(res.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    const getFichas = async () => {
+        try {
+            const res = await getFichasRequest();
+            setFichas(res.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-  const createFicha = async (ficha) => {
-    // console.log("Datos antes de enviar:", ficha);
-    try {
-      const res = await createFichaRequest(ficha);
+    const createFicha = async (ficha) => {
 
-      console.log("Ficha creada:", res);
+        try {
+            const res = await createFichaRequest(ficha);
+            const language = res.data.language;
+            console.log("Ficha creada:", res);
 
-      window.alert(
-        "Postulación enviada con éxito!!!, ahora serás redirigido a la página de inicio"
-      );
-      // console.log('Ficha creada:', res.data);
-    } catch (error) {
-      console.error("Error al crear la ficha:", error);
-    }
-  };
+            let successMessage;
 
-  const deleteFicha = async (id) => {
-    try {
-      const res = await deleteFichaRequest(id);
-      // console.log('Ficha eliminada:', id);
-      window.alert("Ficha eliminada con éxito");
-      if (res.status === 204)
-        setFichas(fichas.filter((ficha) => ficha._id != id));
-    } catch (error) {
-      console.log(error);
-    }
-  };
+            switch (language) {
+                case "es":
+                    successMessage =
+                        "Postulación enviada con éxito!!!, ahora serás redirigido a la página de inicio";
+                    break;
+                case "en":
+                    successMessage =
+                        "Application submitted successfully!!! You will now be redirected to the home page";
+                    break;
+                case "pt":
+                    successMessage =
+                        "Candidatura enviada com sucesso!!! Agora você será redirecionado para a página inicial";
+                    break;
+                default:
+                    successMessage =
+                        "Postulación enviada con éxito!!!, ahora serás redirigido a la página de inicio";
+            }
 
-  const getFicha = async (id) => {
-    try {
-      const res = await getFichaRequest(id);
-      return res.data;
-    } catch (error) {
-      console.log(error);
-    }
-  };
+            window.alert(successMessage);
+        } catch (error) {
+            console.error("Error al crear la ficha:", error);
 
-  const updateFicha = async (id, ficha) => {
-    try {
-      await updateFichaRequest(id, ficha);
-      window.alert("Postulación actualizada con éxito");
-    } catch (error) {
-      console.log(error);
-      window.alert("Error al actualizar ficha");
-    }
-  };
+            let errorMessage;
 
-  return (
-    <FichaContext.Provider
-      value={{
-        fichas,
-        createFicha,
-        getFichas,
-        deleteFicha,
-        getFicha,
-        updateFicha,
-      }}
-    >
-      {children}
-    </FichaContext.Provider>
-  );
+            switch (language) {
+                case "es":
+                    errorMessage = "Error al crear la ficha";
+                    break;
+                case "en":
+                    errorMessage = "Error creating application";
+                    break;
+                case "pt":
+                    errorMessage = "Erro ao criar ficha";
+                    break;
+                default:
+                    errorMessage = "Error al crear la ficha";
+            }
+
+            window.alert(errorMessage);
+        }
+    };
+
+    const deleteFicha = async (id) => {
+        try {
+            const res = await deleteFichaRequest(id);
+            // console.log('Ficha eliminada:', id);
+            window.alert("Ficha eliminada con éxito");
+            if (res.status === 204)
+                setFichas(fichas.filter((ficha) => ficha._id != id));
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const getFicha = async (id) => {
+        try {
+            const res = await getFichaRequest(id);
+            return res.data;
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const updateFicha = async (id, ficha) => {
+        try {
+            await updateFichaRequest(id, ficha);
+            window.alert("Postulación actualizada con éxito");
+        } catch (error) {
+            console.log(error);
+            window.alert("Error al actualizar ficha");
+        }
+    };
+
+    return (
+        <FichaContext.Provider
+            value={{
+                fichas,
+                createFicha,
+                getFichas,
+                deleteFicha,
+                getFicha,
+                updateFicha,
+            }}
+        >
+            {children}
+        </FichaContext.Provider>
+    );
 }
