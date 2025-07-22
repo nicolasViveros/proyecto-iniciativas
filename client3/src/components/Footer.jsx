ed05e672d4a3d77fab5c6eb2c83b665d29efac95
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-22 18:14:12 -0400

    22 julio

--- client3/src/components/Footer.jsx
+++ client3/src/components/Footer.jsx
@@ -3,11 +3,8 @@ import { useAuth } from "../context/AuthContext"
 function Footer() {
   return (
     <div>
-      
-
       <footer className=" w-full md:flex-row ">
-
-        <div className="grid grid-rows-2 ">
+        <div className="grid grid-rows-2 h-fit ">
 
           {/* Fondo restante con imagen de footer */}
           <div className="flex relative overflow-hidden h-32">
