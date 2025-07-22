0076e3fdf1e35c5d9827980544ffd30dd878e0e8
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-22 01:00:49 -0400

    20 jul

--- client3/src/pages/EditFichaPage.jsx
+++ client3/src/pages/EditFichaPage.jsx
@@ -4,6 +4,7 @@ import { useFichas } from '../context/FichasContext';
 import FichaPage from './FichaPage';
 import { useNavigate } from 'react-router-dom';
 import { FaArrowCircleLeft } from "react-icons/fa";
+import LoadingSpinner from "../context/LoadingSpinner";
 
 function EditFichaPage() {
 
