27ab751058be9c2be4006dbc5bd171196671a94e
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-22 00:31:12 -0400

    20 jul

--- client3/src/context/FichasContext.jsx
+++ client3/src/context/FichasContext.jsx
@@ -3,7 +3,7 @@ import { createFichaRequest, getFichasRequest, getFichaRequest, deleteFichaReque
 
 const FichaContext = createContext();
 
-export function loadingSpinner() {
+export const loadingSpinner() {
     return (
         <div
             className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
