b20eb1df55fceba110a4e5cd70396a74b3084e71
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-22 18:07:14 -0400

    22 julio

--- client3/src/components/Footer.jsx
+++ client3/src/components/Footer.jsx
@@ -3,11 +3,11 @@ import { useAuth } from "../context/AuthContext"
 function Footer() {
   return (
     <div>
-
+      
 
       <footer className=" w-full md:flex-row ">
 
-        <div className="grid grid-rows-3 ">
+        <div className="grid grid-rows-2 ">
 
           {/* Fondo restante con imagen de footer */}
           <div className="flex relative overflow-hidden h-32 md:h-auto">
@@ -18,12 +18,10 @@ function Footer() {
             />
           </div>
 
-          <div className="justify-center h-fit flex flex-col sm:flex-row items-center sm:justify-start sm:items-start">
-            <span className="sm:pl-6 text-xs ">Implementado por:</span>
-          </div>
-          
           {/* Logo izquierda (GIZ) con fondo blanco */}
           <div className="flex bg-white w-full md:w-auto px-4 items-start justify-start md:justify-start h-16 md:h-auto">
+            <span className="sm:pl-6 text-xs ">Implementado por:</span>
+
             <Link to="/">
               <img
                 src="/LogoGiz.svg"
