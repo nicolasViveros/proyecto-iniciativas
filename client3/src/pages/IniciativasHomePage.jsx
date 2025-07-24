8c8044a2edfbcd3e2e883cfb87adf5b7d37f04cc
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-24 10:24:07 -0400

    24 julio

--- client3/src/pages/IniciativasHomePage.jsx
+++ client3/src/pages/IniciativasHomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import BannerIniciativas from '../components/BannerIniciativas'
+import MapaConFiltro from '../components/MapaConFiltro'
 
 function IniciativasHomePage() {
     return (
         <div className=' h-[calc(100vh-100px)] bg-red-500'>
        <BannerIniciativas /> 
+       <MapaConFiltro />    
         </div>
     )
 }
