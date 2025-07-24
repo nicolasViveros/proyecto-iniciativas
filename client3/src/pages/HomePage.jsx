c2b12fa66e187ad56a8cf891e994b4b641548de2
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-24 14:44:17 -0400

    24 julio

--- client3/src/pages/HomePage.jsx
+++ client3/src/pages/HomePage.jsx
@@ -1373,9 +1373,7 @@ function HomePage() {
             {secciones[activo].contenido[language]}
           </h2>
         </div>
-      </div>
-
-      <div className="flex flex-col md:flex-row max-w-6xl justify-end gap-4 py-10">
+        <div>
         <button
           onClick={() => navigate("/nueva-ficha")}
           className="bg-[#5d5593] text-white px-4 py-3 rounded-xl hover:bg-[#a49fc4]"
@@ -1413,8 +1411,13 @@ function HomePage() {
               : "Baixar Regulamento"}
           <MdOutlineFileDownload className="text-2xl m-1 mb-1 ml-2 inline" />
         </button>
+        </div>
       </div>
-    </div>
+
+      {/* <div className="flex flex-col md:flex-row max-w-6xl justify-end gap-4 py-10"> */}
+        
+      </div>
+    
   );
 }
 
