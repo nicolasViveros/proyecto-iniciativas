606e3ded4654256d3c29c0dc90c52b57b4cd2670
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-24 10:05:14 -0400

    24 julio

--- client3/src/pages/IniciativasHomePage.jsx
+++ client3/src/pages/IniciativasHomePage.jsx
@@ -4,7 +4,7 @@ function IniciativasHomePage() {
     return (
         <div className=' h-[calc(100vh-100px)] bg-red-500'>
             <div className='flex bg-amber-200 grid-cols-12 gap-4'>
-                <div className='col-[1-5]  bg-blue-300'>text</div>
+                <div className='flex col-[1-5]  bg-blue-300'>text</div>
                 <div className='col-span-7  bg-blue-500'>img</div>             
             </div>
 
