c296c7a8759ea747e31d99472bebe09b03694690
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-24 11:41:45 -0400

    24 julio

--- client3/src/components/Footer.jsx
+++ client3/src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <div>
       <footer className="relative bg-white">
         {/* Imagen de la ciudad como fondo superior */}
-        <div className="w-full">
+        <div className="w-full h-30">
           <img
             src="/Footer2.png"
             alt="Footer"
