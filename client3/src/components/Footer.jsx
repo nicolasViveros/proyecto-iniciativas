3a2c5e661f7a60539921b7b9edb721724a084f3f
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-24 11:59:52 -0400

    24 julio

--- client3/src/components/Footer.jsx
+++ client3/src/components/Footer.jsx
@@ -50,7 +50,7 @@ function Footer() {
               <img
                 src="/LogoGiz.svg"
                 alt="GIZ Logo"
-                className="h-10 md:h-16 lg:h-20"
+                className="h-10 sm:h-13 md:h-17"
               />
             </Link>
           </div>
