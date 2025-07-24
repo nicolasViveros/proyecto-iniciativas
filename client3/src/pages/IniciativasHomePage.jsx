ad9af735a79ed431ddaa6e9a77b5268e9f5bbc71
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-24 09:13:30 -0400

    22 julio

--- client3/src/pages/IniciativasHomePage.jsx
+++ client3/src/pages/IniciativasHomePage.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function IniciativasHomePage() {
   return (
-    <div>IniciativasHomePage</div>
+    <div className='flex h-[calc(100vh-100px)] items-center bg-red-500justify-center'>
+      <div className=' max-w-md w-full p-10  bg-red-200 rounded-md'>
+        <h1 className='text-3xl text-center font-bold mb-4'>Iniciativas</h1>
+      </div>
+    </div>s
   )
 }
 
