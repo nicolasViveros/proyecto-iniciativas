2cb2e28ed5184e523d106d37bd60f537c67cc4b1
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-24 11:46:16 -0400

    24 julio

--- client3/src/components/Footer.jsx
+++ client3/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { useAuth } from "../context/AuthContext"
 function Footer() {
   return (
     <div>
-      <footer className="relative bg-white">
+      <footer className=" bg-white">
         {/* Imagen de la ciudad como fondo superior */}
         <div className="relative w-auto h-30">
           <img
