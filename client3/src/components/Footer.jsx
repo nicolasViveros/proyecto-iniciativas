48d908ee1606c02e2cd9870222b858f4f7460c88
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-24 12:44:44 -0400

    24 julio

--- client3/src/components/Footer.jsx
+++ client3/src/components/Footer.jsx
@@ -49,7 +49,7 @@ function Footer() {
           {/* <div className="relative bg-white w-full md:w-auto px-4 items-justify justify-justify h-20"> */}
           <div className="w-full">
 
-            <div className="max-w-screen-xl mx-auto flex justify-between items-center">
+            <div >
               <span className="flex text-xs  ">Implementado por:</span>
               <span className="flex text-xs  ">Apoyado por:</span>
             </div>
