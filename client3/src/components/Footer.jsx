2595e13282700cc2ffac9710828e392f3c0b81bb
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-24 12:40:57 -0400

    24 julio

--- client3/src/components/Footer.jsx
+++ client3/src/components/Footer.jsx
@@ -33,7 +33,7 @@ function Footer() {
       {/* // </footer> */}
 
 
-      <footer className="h-auto">
+      <footer className="h-full">
         <div className="grid grid-rows-2 ">
           {/* Fondo restante con imagen de footer */}
           <div className=" relative overflow-hidden h-30">
