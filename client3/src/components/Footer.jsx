21d3fa1fef55b1d60a4ca11f2e0282801d274910
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-24 11:47:05 -0400

    24 julio

--- client3/src/components/Footer.jsx
+++ client3/src/components/Footer.jsx
@@ -5,11 +5,11 @@ function Footer() {
     <div>
       <footer className=" bg-white">
         {/* Imagen de la ciudad como fondo superior */}
-        <div className="relative w-auto h-30">
+        <div className="relative overflow-hidden w-auto h-30">
           <img
             src="/Footer2.png"
             alt="Footer"
-            className="w-full h-auto"
+            className="absolute inset-0 w-full h-full object-cover"
           />
         </div>
 
