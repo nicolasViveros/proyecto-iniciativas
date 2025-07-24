54bcf0e1062cb61f007ba2c25f18d8136eee4894
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-24 14:56:34 -0400

    24 julio

--- client3/src/pages/HomePage.jsx
+++ client3/src/pages/HomePage.jsx
@@ -1378,7 +1378,7 @@ function HomePage() {
       <div className="flex flex-col md:flex-row  justify-end-safe gap-4 py-10">
         <button
           onClick={() => navigate("/nueva-ficha")}
-          className="bg-[#5d5593] text-white px-4 py-3 rounded-xl hover:bg-[#a49fc4]"
+          className="bg-[#5d5593] text-white px-4 py-3 right-20 rounded-xl hover:bg-[#a49fc4]"
         >
           {language === "es"
             ? "Comenzar postulación"
