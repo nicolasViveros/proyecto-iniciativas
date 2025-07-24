aa921375d06edf577624f659660ddd5d2982b5fe
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-24 11:28:46 -0400

    24 julio

--- client3/src/components/NavBar.jsx
+++ client3/src/components/NavBar.jsx
@@ -22,7 +22,7 @@ function NavBar() {
                                 <img src="/TRANSPORTE_2.png" alt="Ministerio de Transporte" className="h-10 sm:h-17" />
 
                             </div>
-                            <div className="absolute cols-2 gap-2 top-4 right-4">
+                            <div className="absolute cols-2 gap-2 top-1 right-4">
                             <Link to='/fichas'
                                 className="hover:text-red-400 text-xs">
                                 Fichas |
