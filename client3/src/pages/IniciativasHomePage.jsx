0dd55fdec92d06494ad49d4d8ca190f7298a1e17
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-24 10:07:22 -0400

    24 julio

--- client3/src/pages/IniciativasHomePage.jsx
+++ client3/src/pages/IniciativasHomePage.jsx
@@ -5,12 +5,13 @@ function IniciativasHomePage() {
         <div className=' h-[calc(100vh-100px)] bg-red-500'>
             <div className=' bg-amber-200 grid grid-cols-12 gap-4'>
                 <div className='col-span-5  bg-blue-300'>text</div>
-                <div className='col-span-7  bg-blue-500'>img</div>             
+                <div className='col-span-7  bg-blue-500'>img</div>
             </div>
 
 
             <div className='bg-amber-300 grid place-content-center '>
-                2
+                <div className='col-span-7  bg-blue-300'>map</div>
+                <div className='col-span-5  bg-blue-500'>filter</div>
             </div>
 
 
