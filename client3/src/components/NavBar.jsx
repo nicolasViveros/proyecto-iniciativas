1d51908b2aa1cdcdeeb4769c89710da21a824b22
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-24 11:33:23 -0400

    24 julio

--- client3/src/components/NavBar.jsx
+++ client3/src/components/NavBar.jsx
@@ -47,10 +47,16 @@ function NavBar() {
                     <div className="max-w-screen-xl m-6 flex items-center justify-between">
                         {/* Grupo Izquierdo */}
                         <div className="flex items-center gap-4">
-                            <img src="/Logo-GlobalGateway.svg" alt="Global Gateway" className="h-10 sm:h-17" />
-                            <img src="/Logo-UE.svg" alt="UE" className="h-10 sm:h-17" />
-                            <img src="/Logo-CoperacionAlemana.svg" alt="Cooperación Alemana" className="h-10 sm:h-17" />
-                        </div>
+                                 <Link to="/">
+                                <img src="/Logo-GlobalGateway.svg" alt="Global Gateway" className="h-10 sm:h-17" />
+                                </Link>
+                                <Link to="/">
+                                <img src="/Logo-UE.svg" alt="UE" className="h-10 sm:h-17" />
+                                </Link>
+                                <Link to="/">
+                                <img src="/Logo-CoperacionAlemana.svg" alt="Cooperación Alemana" className="h-10 sm:h-17" />
+                                </Link>
+                            </div>
 
                         {/* Grupo Derecho */}
                         <div className="flex items-center gap-2">
