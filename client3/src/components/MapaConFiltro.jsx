5ad47a4b662787cf0981a333949cdd0c295cfb2e
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-24 17:20:06 -0400

    24 julio

--- client3/src/components/MapaConFiltro.jsx
+++ client3/src/components/MapaConFiltro.jsx
@@ -30,12 +30,11 @@ const MapaConFiltro = () => {
 
   return (
     <div className="flex flex-col md:flex-row items-center justify-between bg-white p-8 rounded-xl shadow-md max-w-6xl mx-auto">
-       <iframe
+      <div className="relative w-full h-screen">
+        <iframe
           src="https://www.openstreetmap.org/export/embed.html"
           className=" container w-auto h-full"
         />
-      <div className="relative w-full h-screen">
-       
 
         {/* Botón para mostrar filtro */}
         {!mostrarFiltro && !paisSeleccionado && (
