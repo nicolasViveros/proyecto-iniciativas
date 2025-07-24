c745f59411ae38079fef9fa70b8362a8a747914c
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-24 10:08:25 -0400

    24 julio

--- client3/src/pages/IniciativasHomePage.jsx
+++ client3/src/pages/IniciativasHomePage.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 function IniciativasHomePage() {
     return (
         <div className=' h-[calc(100vh-100px)] bg-red-500'>
-            <div className=' bg-amber-200 grid grid-cols-12 gap-4'>
+            <div className=' bg-amber-200 grid grid-cols-12 '>
                 <div className='col-span-5  bg-blue-300'>text</div>
                 <div className='col-span-7  bg-blue-500'>img</div>
             </div>
 
 
-            <div className='bg-amber-300 grid place-content-center '>
+            <div className='bg-amber-300 grid grid-cols-12 place-content-center '>
                 <div className='col-span-7  bg-blue-300'>map</div>
                 <div className='col-span-5  bg-blue-500'>filter</div>
             </div>
