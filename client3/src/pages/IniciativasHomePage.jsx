ed185de53689022a6e42848d5b99ffc436c40bbc
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-24 09:51:57 -0400

    22 julio

--- client3/src/pages/IniciativasHomePage.jsx
+++ client3/src/pages/IniciativasHomePage.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
 
 function IniciativasHomePage() {
-  return (
-    <div className=' h-[calc(100vh-100px)] grid-cols-12 gap-4 bg-red-500'>
-      <div className='bg-amber-200 grid place-content-center'>1</div>
-      <div className='bg-amber-300 grid place-content-center '>2</div>
-     
+    return (
+        <div className=' h-[calc(100vh-100px)] grid-cols-12 gap-4 bg-red-500'>
+            <div className='bg-amber-200 grid place-content-center'>
+                <div className='col-span-5 bg-blue-300'>text</div>
+                <div className='col-span-7 bg-blue-500'>img</div>
+                
+            </div>
 
 
-      {/* <div className=' max-w-md w-full p-10  bg-red-200 rounded-md'>
+            <div className='bg-amber-300 grid place-content-center '>
+                2
+            </div>
+
+
+
+            {/* <div className=' max-w-md w-full p-10  bg-red-200 rounded-md'>
         <h1 className='text-3xl text-center font-bold mb-4'>Iniciativas</h1>
       </div> */}
-    </div>
-  )
+        </div>
+    )
 }
 
 export default IniciativasHomePage
