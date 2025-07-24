f9b4027eeef46afd20ee98f6106bce862f3be97c
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-07-24 17:48:11 -0400

    24 julio

--- client3/src/pages/HomePage.jsx
+++ client3/src/pages/HomePage.jsx
@@ -1272,7 +1272,7 @@ function HomePage() {
   return (
     <div className="w-full min-h-screen bg-white font-sans">
       <div className="flex justify-end p-4 space-x-2">
-        <div className="rounded-full border  bg-white ">Cambiar idioma</div>
+        <h1 className="rounded-full border bg-white ">Cambiar idioma</h1>
         <button
           className={`rounded-full p-1.5 border ${language === "es" ? "bg-[#a49fc4]" : "bg-white hover:bg-[#a49fc4]"
             }`}
