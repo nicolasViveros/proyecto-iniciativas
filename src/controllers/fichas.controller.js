fb7025a20f81a51ccad88209139d240e98c3f9b6
Rubén Alvarado <ruben.alvarado@amaris.comm>
2025-07-25 15:25:18 -0400

    cambios en los textos del comprobante de correo

--- src/controllers/fichas.controller.js
+++ src/controllers/fichas.controller.js
@@ -84,40 +84,41 @@ export const createFicha = async (req, res) => {
     let emailSubject, htmlContent, textContent;
     console.log(languageMail);
     switch (languageMail) {
-      case 'en':
+      case "en":
         emailSubject = "Thank you for participating. On the way to Equity";
         htmlContent = `
         <div style="color: #5d5593">
           <h1>Your application has been saved</h1>
-          <p>Thank you for participating in On the way to Equity. Here are the details of your participation:</p>
+          <p>On behalf of Euroclima and SoMoS LAC, we thank you for participating in the contest “Towards Equity”. Here are the details of your application:</p>
           <ul>
             <li><strong>Organization:</strong> ${organizationName}</li>
-            <li><strong>Organization Type:</strong> ${organizationType}</li>
+            <li><strong>Type of organization:</strong> ${organizationType}</li>
             <li><strong>Country:</strong> ${country}</li>
             <li><strong>Legal Representative:</strong> ${legalRepName}</li>
-            <li><strong>Representative's Position:</strong> ${legalRepPosition}</li>
+            <li><strong>Representative’s position:</strong> ${legalRepPosition}</li>
             <li><strong>Email:</strong> ${email}</li>
-            <li><strong>Phone:</strong> ${phone}</li>
+            <li><strong>Phone number:</strong> ${phone}</li>
             <li><strong>Project Name:</strong> ${name}</li>
             <li><strong>City:</strong> ${city}</li>
           </ul>
         </div>
         <img src='https://rumboalaequidad.org/Footer2.png' alt='logo' style="display: block; margin-top: 10px;">
         `;
-        textContent = "Your application has been saved. Thank you for participating in On the way to Equity.";
+        textContent =
+          "Your application has been saved. Thank you for participating in On the way to Equity.";
         break;
-      case 'pt':
+      case "pt":
         emailSubject = "Obrigado por participar. Caminho para a Equidade";
         htmlContent = `
         <div style="color: #5d5593">
           <h1>Sua inscrição foi salva</h1>
-          <p>Obrigado por participar no Caminho para a Equidade. Aqui estão os detalhes da sua participação:</p>
+          <p>Em nome do Euroclima e da SoMoS LAC, agradecemos sua participação no concurso “Rumo à Equidade”. Aqui estão os detalhes da sua participação:</p>
           <ul>
             <li><strong>Organização:</strong> ${organizationName}</li>
             <li><strong>Tipo de Organização:</strong> ${organizationType}</li>
             <li><strong>País:</strong> ${country}</li>
             <li><strong>Representante Legal:</strong> ${legalRepName}</li>
-            <li><strong>Posição do Representante:</strong> ${legalRepPosition}</li>
+            <li><strong>Cargo do representante:</strong> ${legalRepPosition}</li>
             <li><strong>Email:</strong> ${email}</li>
             <li><strong>Telefone:</strong> ${phone}</li>
             <li><strong>Nome do Projeto:</strong> ${name}</li>
@@ -126,14 +127,15 @@ export const createFicha = async (req, res) => {
         </div>
         <img src='https://rumboalaequidad.org/Footer2.png' alt='logo' style="display: block; margin-top: 10px;">
         `;
-        textContent = "Sua inscrição foi salva. Obrigado por participar no Caminho para a Equidade.";
+        textContent =
+          "Sua inscrição foi salva. Obrigado por participar no Caminho para a Equidade.";
         break;
       default: // Default to Spanish
         emailSubject = "Gracias por participar. Rumbo a la Equidad";
         htmlContent = `
         <div style="color: #5d5593">
           <h1>Su postulación ha sido guardada</h1>
-          <p>Gracias por participar en Rumbo a la Equidad. Aquí están los detalles de su participación:</p>
+          <p>En nombre de Euroclima y SoMoS LAC agradecemos su participación en el concurso “Rumbo a la Equidad”. Aquí están los detalles de su participación:</p>
           <ul>
             <li><strong>Organización:</strong> ${organizationName}</li>
             <li><strong>Tipo de Organización:</strong> ${organizationType}</li>
@@ -148,7 +150,8 @@ export const createFicha = async (req, res) => {
         </div>
         <img src='https://rumboalaequidad.org/Footer2.png' alt='logo' style="display: block; margin-top: 10px;">
         `;
-        textContent = "Su postulación ha sido guardada. Gracias por participar en Rumbo a la Equidad.";
+        textContent =
+          "Su postulación ha sido guardada. Gracias por participar en Rumbo a la Equidad.";
     }
 
     try {
