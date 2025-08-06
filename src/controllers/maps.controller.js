e2a1b85204c6286f3baf20fe9539894bf7f41182
Rubén Alvarado <ruben.alvarado@amaris.comm>
2025-08-06 15:14:15 -0400

    add console logs

--- src/controllers/maps.controller.js
+++ src/controllers/maps.controller.js
@@ -10,21 +10,14 @@ export const getGeocodeData = async (req, res) => {
 
   //console.log(address);
   try {
-    console.log(`https://geocode.search.hereapi.com/v1/geocode`, {
-      params: {
-        q: address,
-        apiKey: apiKey,
-      },
-    });
     const response = await axios.get(
-      `https://geocode.search.hereapi.com/v1/geocode`,
-      {
-        params: {
-          q: address,
-          apiKey: apiKey,
-        },
-      }
+      `https://geocode.search.hereapi.com/v1/geocode?q=` +
+        address +
+        `&apiKey=` +
+        apiKey
     );
+
+    console.log("res: mapa", response);
 
     res.json(response.data);
   } catch (error) {
