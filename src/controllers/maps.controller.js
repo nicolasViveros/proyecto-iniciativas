9381ca4100e1bb941bd078c6234d5167f6757b8a
Rubén Alvarado <ruben.alvarado@amaris.comm>
2025-08-06 21:16:52 -0400

    fe

--- src/controllers/maps.controller.js
+++ src/controllers/maps.controller.js
@@ -3,19 +3,9 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export const getGeocodeData = async (address) => {
-  // const address = req.query.req || "";
-  //   console.log("address: ", address);
   const apiKey = process.env.HERE_API_KEY;
-  //   console.log("apiKey: ", apiKey);
 
-  //console.log(address);
   try {
-    // console.log(
-    //   `https://geocode.search.hereapi.com/v1/geocode?q=` +
-    //     address +
-    //     `&apiKey=` +
-    //     apiKey
-    // );
     const response = await axios.get(
       `https://geocode.search.hereapi.com/v1/geocode?q=` +
         address +
@@ -25,8 +15,8 @@ export const getGeocodeData = async (address) => {
 
     console.log("res: mapa", response.data);
 
-    res.json(response.data);
+    return response.data; // Return the data instead of using res
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    throw new Error(error.message); // Throw an error to handle it where the function is called
   }
 };
