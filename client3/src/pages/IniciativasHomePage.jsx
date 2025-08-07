3feefc259c01372c41910eb98f09ae625de74441
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-08-06 20:18:30 -0400

    6 agosto

--- client3/src/pages/IniciativasHomePage.jsx
+++ client3/src/pages/IniciativasHomePage.jsx
@@ -1,22 +1,14 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import BannerIniciativas from '../components/BannerIniciativas'
 import MapaConFiltro from '../components/MapaConFiltro'
-import FichasFilter from '../components/FichasFilter'
-import { useIniciativas } from '../context/IniciativasContext';
 
 function IniciativasHomePage() {
-    const { getIniciativas, iniciativas } = useIniciativas();
-
-    useEffect(() => {
-        getIniciativas();
-    }, []);
-
+  
 
     return (
         <div >
             <BannerIniciativas />
             <MapaConFiltro />
-            <FichasFilter items={iniciativas} type="iniciativa" key={iniciativas._id} />
         </div>
     )
 }
