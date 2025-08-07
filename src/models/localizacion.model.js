fbf0c4636bb42fdf08c7e7b74183461bd02fb37f
Rubén Alvarado <ruben.alvarado@amaris.comm>
2025-08-06 21:26:38 -0400

    unique idIniciativa

--- src/models/localizacion.model.js
+++ src/models/localizacion.model.js
@@ -4,8 +4,8 @@ const localizacionSchema = new mongoose.Schema({
   latitud: { type: Number, required: true },
   longitud: { type: Number, required: true },
   pais: { type: String, required: true },
-  ciudad: { type: String, },
-  idIniciativa: { type: String, required: true },
+  ciudad: { type: String },
+  idIniciativa: { type: String, required: true, unique: true },
 });
 
 export default mongoose.model("Localizacion", localizacionSchema);
