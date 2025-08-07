0bfc2745426fb4ac9d9e4de21fb1f7eef92a4d36
Rubén Alvarado <ruben.alvarado@amaris.comm>
2025-08-07 13:03:30 -0400

    poc here mapa

--- client3/src/components/MapaConFiltro.jsx
+++ client3/src/components/MapaConFiltro.jsx
@@ -224,10 +224,12 @@ const MapaConFiltro = () => {
   return (
     <div className="flex flex-col md:flex-row items-center justify-between bg-white p-8 rounded-xl shadow-md  mx-auto">
       <div className="relative w-full h-150">
-        <Map
-          apikey={"V-p2IrxC_hM0fzfISBie3C0opnL6iu2hxmsVZC56LQY"}
-          iniciativas={iniciativas}
-        />
+        {iniciativas.length > 0 && (
+          <Map
+            apikey={"V-p2IrxC_hM0fzfISBie3C0opnL6iu2hxmsVZC56LQY"}
+            iniciativas={iniciativas}
+          />
+        )}
 
         {!mostrarFiltro && !paisSeleccionado && (
           <Link
