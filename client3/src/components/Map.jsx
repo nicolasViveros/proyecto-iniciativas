3c6267954d22360ae84505ffd839badcebbc1030
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-08-09 19:40:36 -0400

    9 agosto

--- client3/src/components/Map.jsx
+++ client3/src/components/Map.jsx
@@ -109,7 +109,8 @@ const Map = ({ apikey, iniciativas }) => {
       }, true);
     });
   }
-
+ 
+  const [counter, setCounter] = useState(0);
   return (
     <div style={{ position: "relative" }}>
       <div
@@ -139,7 +140,12 @@ const Map = ({ apikey, iniciativas }) => {
           <div>
             {modalData.initiatives.map((initiative) => (
               <div key={initiative._id} className="card my-1">
+                 counter++;
+                 setCounter(counter);
+                 console.log(counter);
+                 <p>counter: {counter}</p>
                 <div className="card-body my-1 border-b border-[#D9D6E1]">
+                  
                   <Link
                     className="card-title cursor-pointer text-lg hover:underline flex mb-1"
                     to={`/iniciativa/${initiative._id}`}
