9c04a77ebab68bda788c4be7ebbf0d3511e6f06b
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-08-09 18:57:16 -0400

    9 agosto

--- client3/src/components/Map.jsx
+++ client3/src/components/Map.jsx
@@ -115,7 +115,7 @@ const Map = ({ apikey, iniciativas }) => {
       <div
         style={{
           width: "100%",
-          height: "auto",
+          height: "100px",
         }}
         ref={mapRef}
       />
