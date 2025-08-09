0edd61818bafc12f7d82f1aec442ef69fcc93e75
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-08-09 18:24:56 -0400

    9 agosto

--- client3/src/components/Map.jsx
+++ client3/src/components/Map.jsx
@@ -13,17 +13,19 @@ const Map = ({ apikey, iniciativas }) => {
     visible: false,
     lat: 0,
     lng: 0,
+    city: '',
+    initiatives: [], // Initialize as empty array
   });
 
   const handlePointerEnter = (location, circle) => {
-    circle.setStyle({ fillColor: "rgba(255, 165, 0, 0.5)" }); // Example style change on hover
+    circle.setStyle({ fillColor: "rgba(255, 165, 0, 0.5)" });
     getIniciativasPorCiudad(location.cityName).then(data => {
       setModalData({
         visible: true,
         lat: circle.getCenter().lat,
         lng: circle.getCenter().lng,
         city: location.cityName,
-        initiatives: data,
+        initiatives: data || [], // Default to empty array if no data
       });
     });
   };
@@ -51,29 +53,6 @@ const Map = ({ apikey, iniciativas }) => {
 
       map.current = newMap;
 
-      const locations = [
-        { lat: -34.6037, lng: -58.3816 }, // Buenos Aires, Argentina
-        { lat: -12.0464, lng: -77.0428 }, // Lima, Peru
-        { lat: -33.4489, lng: -70.6693 }, // Santiago, Chile
-        { lat: -19.9167, lng: -43.9345 }, // Belo Horizonte, Brazil
-        { lat: -34.9011, lng: -56.1645 }, // Montevideo, Uruguay
-        { lat: 4.711, lng: -74.0721 }, // Bogota, Colombia
-        { lat: 6.2442, lng: -75.5812 }, // Medellin, Colombia
-        { lat: -23.5505, lng: -46.6333 }, // São Paulo, Brazil
-        { lat: -22.9068, lng: -43.1729 }, // Rio de Janeiro, Brazil
-        { lat: -0.2299, lng: -78.5249 }, // Quito, Ecuador
-        { lat: -12.0464, lng: -77.0428 }, // Lima, Peru
-        { lat: -15.8267, lng: -47.9218 }, // Brasilia, Brazil
-        { lat: -3.119, lng: -60.0217 }, // Manaus, Brazil
-        { lat: -16.5, lng: -68.15 }, // La Paz, Bolivia
-        { lat: -16.3988, lng: -71.5369 }, // Arequipa, Peru
-        { lat: -2.1704, lng: -79.9224 }, // Guayaquil, Ecuador
-        { lat: -0.1807, lng: -78.4678 }, // Quito, Ecuador
-        { lat: -3.7135, lng: -38.53 }, // Fortaleza, Brazil
-        { lat: -31.7613, lng: -70.5263 }, // Santiago, Chile
-        // Add other locations as needed
-      ];
-
       setTimeout(() => {
         console.log("iniciativas: ", iniciativas);
         createResizableCircles(map.current, iniciativas);
@@ -83,11 +62,10 @@ const Map = ({ apikey, iniciativas }) => {
 
   function createResizableCircles(map, locations) {
     locations.forEach((location) => {
-      console.log("only location:", location);
       if (!location.location) return;
       const position = {
-        lat: location?.location?.latitud,
-        lng: location?.location?.longitud,
+        lat: location.location.latitud,
+        lng: location.location.longitud,
       };
       const circle = new H.map.Circle(position, 85000, {
         style: { fillColor: "rgba(158, 0, 250, 0.7)", lineWidth: 1 },
@@ -109,60 +87,25 @@ const Map = ({ apikey, iniciativas }) => {
 
       map.addObject(circleGroup);
 
-      // Add an event listener for circle tap
-      circleGroup.addEventListener(
-        "tap",
-        function () {
-          //const center = circle.getCenter();
-          //setModalData({ visible: true, lat: center.lat, lng: center.lng });
-          console.log("A idIniciativa: ", location.location.idIniciativa);
-          window.open(
-            `https://rumboalaequidad.org/iniciativa/${location.location.idIniciativa}`
-          );
-        },
-        false
-      );
+      circleGroup.addEventListener("tap", function () {
+        window.open(
+          `https://rumboalaequidad.org/iniciativa/${location.location.idIniciativa}`
+        );
+      }, false);
 
       circleGroup.addEventListener("pointerenter", () => {
         handlePointerEnter(location, circle);
-        console.log(location.ciudad)
       }, true);
 
       circleGroup.addEventListener("pointerleave", () => {
-        // Reset styling or hide the modal
         circle.setStyle({ fillColor: "rgba(158, 0, 250, 0.7)" });
         setModalData((prevData) => ({ ...prevData, visible: false }));
       }, true);
 
-      circleGroup.addEventListener(
-        "pointermove",
-        function (evt) {
-          document.body.style.cursor =
-            evt.target instanceof H.map.Polyline ? "pointer" : "default";
-        },
-        true
-      );
-
-      circleGroup.addEventListener(
-        "drag",
-        function (evt) {
-          const pointer = evt.currentPointer;
-          const distanceFromCenter = circle
-            .getCenter()
-            .distance(map.screenToGeo(pointer.viewportX, pointer.viewportY));
-
-          if (evt.target instanceof H.map.Polyline) {
-            circle.setRadius(distanceFromCenter);
-
-            const outlineGeometry = circle.getGeometry().getExterior();
-            outlineGeometry.pushPoint(outlineGeometry.extractPoint(0));
-            circleOutline.setGeometry(outlineGeometry);
-
-            evt.stopPropagation();
-          }
-        },
-        true
-      );
+      circleGroup.addEventListener("pointermove", function (evt) {
+        document.body.style.cursor =
+          evt.target instanceof H.map.Polyline ? "pointer" : "default";
+      }, true);
     });
   }
 
@@ -217,7 +160,7 @@ const Map = ({ apikey, iniciativas }) => {
           </div>
           <Link
             to="#"
-            onClick={() => setModalData({ visible: false, lat: 0, lng: 0 })}
+            onClick={() => setModalData({ visible: false, lat: 0, lng: 0, initiatives: [] })}
             className="absolute top-1 right-1 p-2 hover:text-[#a49fc4]"
           >
             <FaWindowClose className="text-xl" />
