bfe472bdfc910ed68def612d541f53db083718fb
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-08-09 19:16:36 -0400

    9 agosto

--- client3/src/components/Map.jsx
+++ client3/src/components/Map.jsx
@@ -136,7 +136,7 @@ const Map = ({ apikey, iniciativas }) => {
           }}
         >
           <h3>{modalData.city}</h3>
-          <div className=" h-full z-20 overflow-y-full">
+          <div className="">
             {modalData.initiatives.map((initiative) => (
               <div key={initiative._id} className="card my-1">
                 <div className="card-body my-1 border-b border-[#D9D6E1]">
