3e78937ee815c75ce9801849e0efbdcc5bbfa555
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-08-10 08:27:04 -0400

    9 agosto

--- client3/src/pages/IniciativasPage.jsx
+++ client3/src/pages/IniciativasPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { useEffect,  } from 'react'
+import { useEffect, } from 'react'
 import { useIniciativas } from '../context/IniciativasContext'
 import { Link } from 'react-router-dom'; // Asegúrate de tener react-router-dom instalado
 import LoadingSpinner from "../context/LoadingSpinner";
 import FichasFilter from '../components/FichasFilter';
+import { useAuth } from "../context/AuthContext";
 
 // function IniciativaCard({ iniciativa, handleDelete }) {
 //     return (
@@ -16,7 +17,7 @@ import FichasFilter from '../components/FichasFilter';
 //                 </h2>
 //                 <p><strong>País:</strong> {iniciativa.pais}</p>
 //                 <p><strong>Ciudad:</strong> {iniciativa.ciudad}</p>
-            
+
 
 //                 <div className=" flex gap-x-2 mb-1 items-end justify-end">
 //                     <button
@@ -44,44 +45,63 @@ import FichasFilter from '../components/FichasFilter';
 
 
 function IniciativasPage() {
+  const { isAuthenticated } = useAuth();
+  const { getIniciativas, iniciativas } = useIniciativas();
+  // const [isSaving, setIsSaving] = useState(false);
+  // const navigate = useNavigate();
 
-    const { getIniciativas, iniciativas } = useIniciativas();
-    // const [isSaving, setIsSaving] = useState(false);
-    // const navigate = useNavigate();
-  
-    // const handleDelete = async (id) => {
-    //   setIsSaving(true);
-    //   try {
-    //     await deleteIniciativa(id);
-    //     navigate('/iniciativas');
-    //   } catch (error) {
-    //     console.error("Error al eliminar la iniciativa:", error);
-    //   } finally {
-    //     setIsSaving(false);
-    //   }
-    // }; 
-    useEffect(() => {
-      getIniciativas();
-    }, []);
-  
-    if (iniciativas.length == 0) return (<div className=" flex  items-center justify-center min-h-screen">
-      {/* <div className="max-w-3xl w-full rounded-md justify-center items-center"> */}
-        Cargando iniciativas...
-        <LoadingSpinner />
-      {/* </div> */}
-    </div>);
-  
-    // if (isSaving) {
-    //   return <div className="flex items-center justify-center min-h-screen">
-    //     {/* <div className="max-w-3xl w-full rounded-md justify-center items-center"> */}
-    //       Eliminando iniciativa...
-    //       <LoadingSpinner />
-    //     {/* </div> */}
-    //   </div>;
-    // }
+  // const handleDelete = async (id) => {
+  //   setIsSaving(true);
+  //   try {
+  //     await deleteIniciativa(id);
+  //     navigate('/iniciativas');
+  //   } catch (error) {
+  //     console.error("Error al eliminar la iniciativa:", error);
+  //   } finally {
+  //     setIsSaving(false);
+  //   }
+  // }; 
+  useEffect(() => {
+    getIniciativas();
+  }, []);
 
-    return (
+  if (iniciativas.length == 0) return (<div className=" flex  items-center justify-center min-h-screen">
+    {/* <div className="max-w-3xl w-full rounded-md justify-center items-center"> */}
+    Cargando iniciativas...
+    <LoadingSpinner />
+    {/* </div> */}
+  </div>);
+
+  // if (isSaving) {
+  //   return <div className="flex items-center justify-center min-h-screen">
+  //     {/* <div className="max-w-3xl w-full rounded-md justify-center items-center"> */}
+  //       Eliminando iniciativa...
+  //       <LoadingSpinner />
+  //     {/* </div> */}
+  //   </div>;
+  // }
+
+  return (
+
+    <div className="relative flex items-center justify-center min-h-screen">
+      <div className="min-h-screen bg-gray-100 p-4"></div>
       <FichasFilter items={iniciativas} type="iniciativa" key={iniciativas._id} />
+
+      {
+        isAuthenticated ? (
+          <div className="absolute top-4 right-4 ">
+            <Link
+              to={`/iniciativa/${iniciativas._id}/editar`}
+              className="bg-[#5d5593] text-white px-4 py-2 rounded hover:bg-[#a49fc4]"
+            >
+              editar
+            </Link>
+          </div>
+        ) : (<div className="absolute top-4 right-4 ">
+
+        </div>)
+      }
+    </div>
     );
 
 }
