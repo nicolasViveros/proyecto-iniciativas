093a73439d397547aa840e63af99cdb103524db8
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-08-09 22:15:14 -0400

    9 agosto

--- client3/src/components/MapaConFiltro.jsx
+++ client3/src/components/MapaConFiltro.jsx
@@ -186,6 +186,61 @@ const MapaConFiltro = () => {
             ))}
           </div>
         )}
+
+        {!paisSeleccionado && iniciativasFilter.length > 0 && (
+          <div className="absolute top-0 right-0 h-full bg-white shadow-lg w-104 z-20 overflow-y-auto">
+            <div className="flex justify-between items-center p-4 border-b border-[#D9D6E1]">
+              <button
+                onClick={() => {
+                  setIniciativas([]); // Clear initiatives to update state
+                  setMostrarFiltro(true);
+                }}
+                className="text-xl font-semibold cursor-pointer hover:underline"
+              >
+                <LuCircleArrowLeft className="text-2xl inline hover:text-[#a49fc4]" />
+              </button>
+
+              <h3 className="text-xl font-semibold">Iniciativas en la ciudad seleccionada</h3>
+              <button
+                onClick={() => {
+                  setIniciativas([]);
+                  setMostrarFiltro(false);
+                }}
+                className="border border-[#5d5593] text-[#5d5593] px-4 py-2 rounded-xl hover:bg-[#a49fc4] z-10"
+              >
+                Ocultar
+                <FaAngleRight className="text-xl inline" />
+              </button>
+            </div>
+            <div className="p-4 text-sm py-2 space-y-2">
+              {iniciativasFilter.map((iniciativa) => (
+                <div key={iniciativa._id} className="card my-2">
+                  <div className="card-body my-1">
+                    <Link
+                      className="card-title cursor-pointer text-lg hover:underline flex mb-2"
+                      to={`/iniciativa/${iniciativa._id}`}
+                    >
+                      {iniciativa.nombreIniciativa}
+                    </Link>
+                    <p className="text-sm font-bold">{iniciativa.ciudad}</p>
+                    <span className="mt-1 mb-4 text-sm rounded truncated-text">
+                      {iniciativa.descripcionIniciativa}
+                    </span>
+                    <Link
+                      className=" bottom-1 right-1 flex justify-end hover:text-[#a49fc4] rounded-md underline"
+                      to={`/iniciativa/${iniciativa._id}`}
+                    >
+                      <span className="ml-1">Revisar iniciativa</span>
+                      <FaEye className="text-2xl ml-1 mb-1 inline" />
+                    </Link>
+                    <hr className="border-t-2 border-gray-300" />
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+        ƒ
       </div>
     </div>
   );
