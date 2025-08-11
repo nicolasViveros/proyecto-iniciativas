7b8645fa776d74819ce49bc780d000e0179eb7c7
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-08-11 14:00:27 -0400

    9 agosto

--- client3/src/components/MapaConFiltro.jsx
+++ client3/src/components/MapaConFiltro.jsx
@@ -77,68 +77,68 @@ const MapaConFiltro = () => {
 
   return (
     <div className="flex flex-col md:flex-row items-center justify-between bg-white p-8 rounded-xl shadow-md mx-auto">
-    <div className="relative w-full h-150">
-      {iniciativas.length > 0 && (
-        <Map
-          apikey={"V-p2IrxC_hM0fzfISBie3C0opnL6iu2hxmsVZC56LQY"}
-          iniciativas={iniciativas}
-          onCitySelect={(city) => cargarIniciativas("ciudad", city)}
-        />
-      )}
+      <div className="relative w-full h-150">
+        {iniciativas.length > 0 && (
+          <Map
+            apikey={"V-p2IrxC_hM0fzfISBie3C0opnL6iu2hxmsVZC56LQY"}
+            iniciativas={iniciativas}
+            onCitySelect={(city) => cargarIniciativas("ciudad", city)}
+          />
+        )}
 
-      {!mostrarFiltro && !paisSeleccionado && !ciudadSeleccionado && (
-        <Link
-          className="absolute top-0 right-0 mt-2 mr-2 bg-white border border-[#5d5593] text-[#5d5593] px-4 py-2 rounded-xl hover:bg-[#a49fc4] z-10"
-          onClick={mostrarFiltroRegiones}
-        >
-          <FaAngleLeft className="text-xl inline" />
-          <span className="ml-1">Mostrar filtro</span>
-        </Link>
-      )}
+        {!mostrarFiltro && !paisSeleccionado && !ciudadSeleccionado && (
+          <Link
+            className="absolute top-0 right-0 mt-2 mr-2 bg-white border border-[#5d5593] text-[#5d5593] px-4 py-2 rounded-xl hover:bg-[#a49fc4] z-10"
+            onClick={mostrarFiltroRegiones}
+          >
+            <FaAngleLeft className="text-xl inline" />
+            <span className="ml-1">Mostrar filtro</span>
+          </Link>
+        )}
 
-      {mostrarFiltro && (
-        <div className="absolute top-0 right-0 h-full bg-white shadow-lg w-104 overflow-hidden z-10">
-          <div className="flex justify-between items-center p-2 border-b border-[#D9D6E1]">
-            <h2 className="text-xl font-semibold">Iniciativas</h2>
-            <button
-              onClick={mostrarFiltroRegiones}
-              className="border border-[#5d5593] text-[#5d5593] px-4 py-2 rounded-xl hover:bg-[#a49fc4] z-10"
-            >
-              Ocultar
-              <FaAngleRight className="text-xl inline" />
-            </button>
+        {mostrarFiltro && (
+          <div className="absolute top-0 right-0 h-full bg-white shadow-lg w-104 overflow-hidden z-10">
+            <div className="flex justify-between items-center p-2 border-b border-[#D9D6E1]">
+              <h2 className="text-xl font-semibold">Iniciativas</h2>
+              <button
+                onClick={mostrarFiltroRegiones}
+                className="border border-[#5d5593] text-[#5d5593] px-4 py-2 rounded-xl hover:bg-[#a49fc4] z-10"
+              >
+                Ocultar
+                <FaAngleRight className="text-xl inline" />
+              </button>
+            </div>
+
+            <div className="p-3 overflow-y-auto h-[calc(100%-40px)]">
+              {Object.entries(regiones).map(([region, paises]) => (
+                <div key={region} className="mb-4 border-b border-[#D9D6E1]">
+                  <button
+                    className="flex justify-between items-center w-full text-lg font-semibold hover:underline"
+                    onClick={() =>
+                      setRegionActiva(regionActiva === region ? null : region)
+                    }
+                  >
+                    <span>{region}</span>
+                    <FaAngleDown className="text-xl inline" />
+                  </button>
+                  {regionActiva === region && (
+                    <ul className="pl-3 pt-2 text-base space-y-1">
+                      {paises.map((pais) => (
+                        <li
+                          key={pais}
+                          className="cursor-pointer hover:underline"
+                          onClick={() => cargarIniciativas("pais", pais)}
+                        >
+                          {pais}
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </div>
+              ))}
+            </div>
           </div>
-
-          <div className="p-3 overflow-y-auto h-[calc(100%-40px)]">
-            {Object.entries(regiones).map(([region, paises]) => (
-              <div key={region} className="mb-4 border-b border-[#D9D6E1]">
-                <button
-                  className="flex justify-between items-center w-full text-lg font-semibold hover:underline"
-                  onClick={() =>
-                    setRegionActiva(regionActiva === region ? null : region)
-                  }
-                >
-                  <span>{region}</span>
-                  <FaAngleDown className="text-xl inline" />
-                </button>
-                {regionActiva === region && (
-                  <ul className="pl-3 pt-2 text-base space-y-1">
-                    {paises.map((pais) => (
-                      <li
-                        key={pais}
-                        className="cursor-pointer hover:underline"
-                        onClick={() => cargarIniciativas("pais", pais)}
-                      >
-                        {pais}
-                      </li>
-                    ))}
-                  </ul>
-                )}
-              </div>
-            ))}
-          </div>
-        </div>
-      )}
+        )}
 
         {paisSeleccionado ? (
           <div className="absolute top-0 right-0 h-full bg-white shadow-lg w-104 z-20 overflow-y-auto">
@@ -216,7 +216,7 @@ const MapaConFiltro = () => {
               <button
                 onClick={() => {
                   setIniciativas([]); // Clear initiatives to update state
-                  setMostrarFiltro(true);
+                  mostrarFiltroRegiones
                 }}
                 className="text-xl font-semibold cursor-pointer hover:underline"
               >
@@ -263,7 +263,7 @@ const MapaConFiltro = () => {
             </div>
           </div>
         )}
-        
+
       </div>
     </div>
   );
