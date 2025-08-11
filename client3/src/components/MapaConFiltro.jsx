2a584df5608a3ddf6b5c003f25f1f78cbaf3f335
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-08-11 14:42:19 -0400

    9 agosto

--- client3/src/components/MapaConFiltro.jsx
+++ client3/src/components/MapaConFiltro.jsx
@@ -144,6 +144,7 @@ const MapaConFiltro = () => {
                 onClick={() => {
                   setMostrarFiltro(false);
                   setCiudadSeleccionado(null);
+                  setPaisSeleccionado(null);
                 }}
                 className="border border-[#5d5593] text-[#5d5593] px-4 py-2 rounded-xl hover:bg-[#a49fc4] z-10"
               >
@@ -211,6 +212,7 @@ const MapaConFiltro = () => {
                 onClick={() => {
                   setMostrarFiltro(false);
                   setPaisSeleccionado(null);
+                  setCiudadSeleccionado(null);
                 }}
                 className="border border-[#5d5593] text-[#5d5593] px-4 py-2 rounded-xl hover:bg-[#a49fc4] z-10"
               >
