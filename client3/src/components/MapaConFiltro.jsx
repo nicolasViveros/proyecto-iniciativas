1ce554de226868992ff793a99873ad3bcb16d41c
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-08-11 16:32:00 -0400

    9 agosto

--- client3/src/components/MapaConFiltro.jsx
+++ client3/src/components/MapaConFiltro.jsx
@@ -28,10 +28,10 @@ const MapaConFiltro = () => {
   const [mostrarFiltro, setMostrarFiltro] = useState(false);
   const [regionActiva, setRegionActiva] = useState(null);
   const [paisSeleccionado, setPaisSeleccionado] = useState(null);
-  const [ciudadSeleccionado, setCiudadSeleccionado] = useState(null); // Usar esta variable para mostrar el mapa de la ciudad seleccionada
+  const [ciudadSeleccionado, setCiudadSeleccionado] = useState(null);
 
   const mostrarFiltroRegiones = () => {
-    setMostrarFiltro(!mostrarFiltro);
+    setMostrarFiltro((prevState) => !prevState);
   };
 
   const cargarIniciativas = (tipo, value) => {
@@ -40,21 +40,21 @@ const MapaConFiltro = () => {
       getIniciativasPorPais(value).then((data) => {
         setIniciativas(data);
         setMostrarFiltro(true);
-        setCiudadSeleccionado(null); // Ensure no city is selected
+        setCiudadSeleccionado(null);
       });
     } else if (tipo === "ciudad") {
       setCiudadSeleccionado(value);
       getIniciativasPorCiudad(value).then((data) => {
         setIniciativas(data);
         setMostrarFiltro(true);
-        setPaisSeleccionado(null); // Ensure no country is selected
+        setPaisSeleccionado(null);
       });
     }
   };
 
   useEffect(() => {
     getIniciativas();
-  }, []);
+  }, [getIniciativas]);
 
   return (
     <div className="flex flex-col md:flex-row items-center justify-between bg-white p-8 rounded-xl shadow-md mx-auto">
@@ -77,18 +77,21 @@ const MapaConFiltro = () => {
           </Link>
         )}
 
-        {/* {mostrarFiltro && (
+        {mostrarFiltro && (
           <div className="absolute top-0 right-0 h-full bg-white shadow-lg w-104 overflow-hidden z-10">
             <div className="flex justify-between items-center p-2 border-b border-[#D9D6E1]">
-              <h2 className="text-xl font-semibold">Iniciativas en 2</h2>
+              <h2 className="text-xl font-semibold">
+                {paisSeleccionado
+                  ? `Iniciativas en ${paisSeleccionado}`
+                  : `Iniciativas ${ciudadSeleccionado ? "en Ciudad" : ""}`}
+              </h2>
               <button
                 onClick={() => {
-                  setMostrarFiltro(false),
-                    setCiudadSeleccionado(null),
-                    setPaisSeleccionado(null)
-                }
-                }
-
+                  setMostrarFiltro(false);
+                  setCiudadSeleccionado(null);
+                  setPaisSeleccionado(null);
+                  setIniciativas([]);
+                }}
                 className="border border-[#5d5593] text-[#5d5593] px-4 py-2 rounded-xl hover:bg-[#a49fc4] z-10"
               >
                 Ocultar
@@ -125,99 +128,32 @@ const MapaConFiltro = () => {
               ))}
             </div>
           </div>
-        )} */}
+        )}
 
-        {paisSeleccionado ? (
+        {(paisSeleccionado || ciudadSeleccionado) && (
           <div className="absolute top-0 right-0 h-full bg-white shadow-lg w-104 z-20 overflow-y-auto">
             <div className="flex justify-between items-center p-4 border-b border-[#D9D6E1]">
-              <Link
+              <button
                 onClick={() => {
                   setIniciativas([]);
                   mostrarFiltroRegiones();
+                  setPaisSeleccionado(null);
+                  setCiudadSeleccionado(null);
                 }}
                 className="text-xl font-semibold cursor-pointer hover:underline"
               >
                 <LuCircleArrowLeft className="text-2xl inline hover:text-[#a49fc4]" />
-              </Link>
+              </button>
 
               <h3 className="text-xl font-semibold">
-                Iniciativas en {paisSeleccionado}
+                Iniciativas en {paisSeleccionado || ciudadSeleccionado}
               </h3>
-              <button
-                onClick={() => {
-                  setMostrarFiltro(false),
-                  setCiudadSeleccionado(null),
-                  setPaisSeleccionado(null)
-
-                }}
-                className="border border-[#5d5593] text-[#5d5593] px-4 py-2 rounded-xl hover:bg-[#a49fc4] z-10"
-              >
-                Ocultar
-                <FaAngleRight className="text-xl inline" />
-              </button>
-            </div>
-            <div className="p-4 text-sm py-2 space-y-2">
-              {iniciativasFilter.map((iniciativa) => (
-                <div key={iniciativa._id} className="card my-2">
-                  <div className="card-body my-1">
-                    <Link
-                      className="card-title cursor-pointer text-lg hover:underline flex mb-2"
-                      to={`/iniciativa/${iniciativa._id}`}
-                    >
-                      {iniciativa.nombreIniciativa}
-                    </Link>
-                    {iniciativa.ciudad ? (
-                      <p className="text-sm font-bold">{iniciativa.ciudad}</p>
-                    ) : (
-                      <p className="text-sm font-bold">{iniciativa.alcance}</p>
-                    )}
-                    <span className="mt-1 mb-4 text-sm rounded truncated-text">
-                      {iniciativa.descripcionIniciativa}
-                    </span>
-                    <Link
-                      className=" bottom-1 right-1 flex justify-end hover:text-[#a49fc4] rounded-md underline"
-                      to={`/iniciativa/${iniciativa._id}`}
-                    >
-                      <span className="ml-1">Revisar iniciativa</span>
-                      <FaEye className="text-2xl ml-1 mb-1 inline" />
-                    </Link>
-                    <hr className="border-t-2 border-gray-300" />
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-        ) : (
-          <div className="p-4 text-sm py-2 space-y-2">
-            {iniciativasFilter.map((iniciativa) => (
-              <div key={iniciativa._id} className="card my-2">
-                <div className="card-body my-1">
-                  {/* Initiative display code */}
-                </div>
-              </div>
-            ))}
-          </div>
-        )}
-
-        {!paisSeleccionado && iniciativasFilter.length > 0 && (
-          <div className="absolute top-0 right-0 h-full bg-white shadow-lg w-104 z-20 overflow-y-auto">
-            <div className="flex justify-between items-center p-4 border-b border-[#D9D6E1]">
-              <button
-                onClick={() => {
-                  setIniciativas([]); // Clear initiatives to update state
-                  mostrarFiltroRegiones
-                }}
-                className="text-xl font-semibold cursor-pointer hover:underline"
-              >
-                <LuCircleArrowLeft className="text-2xl inline hover:text-[#a49fc4]" />
-              </button>
-
-              <h3 className="text-xl font-semibold">Iniciativas en {ciudadSeleccionado}</h3>
               <button
                 onClick={() => {
                   setMostrarFiltro(false);
                   setPaisSeleccionado(null);
                   setCiudadSeleccionado(null);
+                  setIniciativas([]);
                 }}
                 className="border border-[#5d5593] text-[#5d5593] px-4 py-2 rounded-xl hover:bg-[#a49fc4] z-10"
               >
@@ -235,7 +171,7 @@ const MapaConFiltro = () => {
                     >
                       {iniciativa.nombreIniciativa}
                     </Link>
-                    <p className="text-sm font-bold">{iniciativa.ciudad}</p>
+                    <p className="text-sm font-bold">{iniciativa.ciudad || iniciativa.alcance}</p>
                     <span className="mt-1 mb-4 text-sm rounded truncated-text">
                       {iniciativa.descripcionIniciativa}
                     </span>
@@ -253,9 +189,8 @@ const MapaConFiltro = () => {
             </div>
           </div>
         )}
-
       </div>
-    </div >
+    </div>
   );
 };
 
