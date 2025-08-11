845ee8bc43a0701731981fb4c2250c25ad288506
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-08-11 13:22:47 -0400

    9 agosto

--- client3/src/pages/IniciativaPage.jsx
+++ client3/src/pages/IniciativaPage.jsx
@@ -46,7 +46,10 @@ function IniciativaPage() {
           <div className="absolute top-4 right-4 ">
             <Link
               onClick={() => {
+                const userConfirmed = window.confirm("¿Está seguro de que desea eliminar esta iniciativa? Esta acción no se puede deshacer.");
+                if (!userConfirmed){
                 deleteIniciativa(iniciativa._id);
+              }
               }} className="bg-[#5d5593] text-white px-4 py-2 mr-2 rounded hover:bg-[#a49fc4]"
             >
               Eliminar
