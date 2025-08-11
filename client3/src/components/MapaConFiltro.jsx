ab3472c4a7bedf152bc84c3ef1a05d4b17d67183
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-08-11 13:45:54 -0400

    9 agosto

--- client3/src/components/MapaConFiltro.jsx
+++ client3/src/components/MapaConFiltro.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { FaAngleLeft, FaAngleRight, FaAngleDown } from "react-icons/fa";
 import { LuCircleArrowLeft } from "react-icons/lu";
 import { Link } from "react-router-dom";
@@ -64,7 +64,7 @@ const MapaConFiltro = () => {
           />
         )}
 
-        {!mostrarFiltro && !paisSeleccionado && !ciudadSeleccionado && (
+        {!mostrarFiltro (
           <Link
             className="absolute top-0 right-0 mt-2 mr-2 bg-white border border-[#5d5593] text-[#5d5593] px-4 py-2 rounded-xl hover:bg-[#a49fc4] z-10"
             onClick={toggleFiltro}
