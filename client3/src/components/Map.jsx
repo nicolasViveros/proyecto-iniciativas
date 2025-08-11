00c3c64005bbfdeec764fde36ededd6ac02abec5
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-08-11 16:50:57 -0400

    9 agosto

--- client3/src/components/Map.jsx
+++ client3/src/components/Map.jsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
 import H from "@here/maps-api-for-javascript";
 import { Link } from "react-router-dom";
-import { FaEye, FaWindowClose } from "react-icons/fa";
+import { FaWindowClose } from "react-icons/fa";
 import { useIniciativas } from "../context/IniciativasContext";
-import { paisesCoordinates } from "../components/MapaConFiltro";
 
 const Map = ({ apikey, iniciativas, onCitySelect, center }) => {
   const { getIniciativasPorCiudad } = useIniciativas();
@@ -28,7 +27,6 @@ const Map = ({ apikey, iniciativas, onCitySelect, center }) => {
         city: location.ciudad,
         initiatives: data || [], // Default to empty array if no data
       });
-      console.log(modalData);
     });
   };
 
@@ -39,28 +37,33 @@ const Map = ({ apikey, iniciativas, onCitySelect, center }) => {
       const defaultLayers = platform.current.createDefaultLayers({
         pois: true,
       });
-      const newMap = new H.Map(
+      map.current = new H.Map(
         mapRef.current,
         defaultLayers.vector.normal.map,
         {
-          zoom: 4, // Set zoom level
-          center: center || paisesCoordinates["Argentina"], // Set map center
+          zoom: 4,
+          center: center || { lat: -34.6037, lng: -58.3816 }, // Default center, e.g., Argentina
         }
       );
 
-      const behavior = new H.mapevents.Behavior(
-        new H.mapevents.MapEvents(newMap)
-      );
-      const ui = H.ui.UI.createDefault(newMap, defaultLayers);
-
-      map.current = newMap;
-
-      setTimeout(() => {
-        console.log("iniciativas: ", iniciativas);
-        createResizableCircles(map.current, iniciativas);
-      }, 3000);
+      new H.mapevents.Behavior(new H.mapevents.MapEvents(map.current));
+      H.ui.UI.createDefault(map.current, defaultLayers);
     }
-  }, [apikey, getIniciativasPorCiudad]);
+
+    // Update the map center when the `center` prop changes
+    if (center && map.current) {
+      map.current.setCenter(center);
+    }
+
+  }, [apikey, center, getIniciativasPorCiudad]);
+
+  useEffect(() => {
+    if (map.current && iniciativas) {
+      map.current.removeObjects(map.current.getObjects());
+      createResizableCircles(map.current, iniciativas);
+    }
+  }, [iniciativas]);
+
 
   function createResizableCircles(map, locations) {
     locations.forEach((location) => {
@@ -84,15 +87,14 @@ const Map = ({ apikey, iniciativas, onCitySelect, center }) => {
         objects: [circle, circleOutline],
       });
 
-      circleOutline
-        .getGeometry()
+      circleOutline.getGeometry()
         .pushPoint(circleOutline.getGeometry().extractPoint(0));
 
       map.addObject(circleGroup);
 
-      circleGroup.addEventListener("tap", function () {
+      circleGroup.addEventListener("tap", () => {
         if (onCitySelect) {
-          onCitySelect(location.ciudad); // Call the function from the prop
+          onCitySelect(location.ciudad);
         }
       }, false);
 
