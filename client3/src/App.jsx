fc566d2cfc9e13c5e2c811c0a95c4a449224285f
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-08-13 14:05:36 -0400

    13 agosto

--- client3/src/App.jsx
+++ client3/src/App.jsx
@@ -51,7 +51,7 @@ export function App() {
                     <Route path="/ficha/:id/editar" element={<EditFichaPage />} />
                     <Route path="/iniciativasPorPais/:pais" element={<IniciativasPage />} />
                     <Route path="/iniciativasPorCiudad/:ciudad" element={<IniciativasPage />} />
-                    <Route path="/iniciativasPorLocalidad/:localidad" element={<IniciativasPage />} />
+                    <Route path="/locationPorIniciativa/:id" element={<IniciativasPage />} />
                   </Route>
                 </Routes>
 
