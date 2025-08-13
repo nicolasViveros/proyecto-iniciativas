ce318ea33e11a59ee67b1495d02324df79e19176
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-08-13 14:29:25 -0400

    13 agosto

--- client3/src/components/InitiativeCard.jsx
+++ client3/src/components/InitiativeCard.jsx
@@ -38,7 +38,7 @@ const InitiativeCard = ({ iniciativa }) => {
 
   const navigate = useNavigate();
   console.log(iniciativa._id)
-  console.log(location)
+  console.log(location.longitud)
 
   return (
     <div className="flex flex-col lg:flex-row bg-gray-50 rounded-lg shadow-xl overflow-hidden p-6 max-w-6xl mx-auto my-8">
