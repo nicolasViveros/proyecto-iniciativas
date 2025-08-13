7eec30ae65239d5a1081222718713a7f4629cd2b
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-08-13 14:27:21 -0400

    13 agosto

--- client3/src/components/InitiativeCard.jsx
+++ client3/src/components/InitiativeCard.jsx
@@ -28,12 +28,13 @@ const InitiativeCard = ({ iniciativa }) => {
   const { getLocationPorIniciativa } = useIniciativas();
 
   useEffect(() => {
-    
+    if (_id) {
       getLocationPorIniciativa(_id).then((data) => {
+        console.log(data);
         setLocation(data);
       });
-    
-  }, []);
+    }
+  }, [_id]);
 
   const navigate = useNavigate();
   console.log(iniciativa._id)
