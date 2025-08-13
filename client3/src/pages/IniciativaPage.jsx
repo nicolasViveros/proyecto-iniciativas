a811a5d413bd35ff09af4ab4b18b5ca051b88141
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-08-13 17:23:21 -0400

    13 agosto

--- client3/src/pages/IniciativaPage.jsx
+++ client3/src/pages/IniciativaPage.jsx
@@ -99,12 +99,12 @@ const actualizaLocalizacion = async () => {
 
           {isAuthenticated ? (
             <div className="absolute top-4 right-4 ">
-              {/* <Link
+              <Link
                 onClick={actualizaLocalizacion}
                 className="bg-[#5d5593] text-white px-4 py-2 mr-2 rounded hover:bg-[#a49fc4]"
               >
                 Actualiza Localización
-              </Link> */}
+              </Link>
               <Link
                 onClick={eliminaIniciativa}
                 className="bg-[#5d5593] text-white px-4 py-2 mr-2 rounded hover:bg-[#a49fc4]"
