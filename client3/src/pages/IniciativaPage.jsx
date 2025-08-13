60395ed54a11b31e3c1b7da63b57811eb911a43c
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-08-13 13:26:52 -0400

    13 agosto

--- client3/src/pages/IniciativaPage.jsx
+++ client3/src/pages/IniciativaPage.jsx
@@ -107,7 +107,7 @@ function IniciativaPage() {
           {isAuthenticated ? (
             <div className="absolute top-4 right-4 ">
                <Link
-                onClick={actualizaLocalizacion}
+                // onClick={actualizaLocalizacion}
                 className="bg-[#5d5593] text-white px-4 py-2 mr-2 rounded hover:bg-[#a49fc4]"
               >
                 Actualizar localización
