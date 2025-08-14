8008536f13dd4d4f561725b46b83d05702245ea6
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-08-14 15:25:47 -0400

    13 agosto

--- client3/src/pages/IniciativaPage.jsx
+++ client3/src/pages/IniciativaPage.jsx
@@ -113,7 +113,7 @@ function IniciativaPage() {
               Menú
             </button>
             {menuOpen && (
-              <div className="mt-2 bg-white border rounded shadow-lg">
+              <div className="absolute right-0 mt-2 bg-white border rounded shadow-lg">
                 <Link
                   onClick={actualizaLocalizacion}
                   className="block px-4 py-2 text-[#5d5593] hover:bg-[#ebe9f6]"
