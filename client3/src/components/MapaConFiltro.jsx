f0ed6332baf9fbfc01a780ec0ffb1cb318f97366
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-08-14 14:04:22 -0400

    13 agosto

--- client3/src/components/MapaConFiltro.jsx
+++ client3/src/components/MapaConFiltro.jsx
@@ -59,7 +59,7 @@ const MapaConFiltro = () => {
 
   return (
     <div className="flex flex-col md:flex-row items-center justify-between bg-white p-8 rounded-xl shadow-md mx-auto">
-      <div className="relative w-full h-150">
+      <div className="relative w-full h-50 sm:h-150">
         {iniciativas.length > 0 && (
           <Map
             apikey={"V-p2IrxC_hM0fzfISBie3C0opnL6iu2hxmsVZC56LQY"}
