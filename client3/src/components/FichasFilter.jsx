74a8562361aa91f0cbca5a125121bc1e3e17144e
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-08-14 20:54:01 -0400

    13 agosto

--- client3/src/components/FichasFilter.jsx
+++ client3/src/components/FichasFilter.jsx
@@ -54,7 +54,7 @@ export default function FichasFilter({ items, type = "ficha" }) {
                 {type === "ficha" ? (
                   <p className="text-sm font-bold">{item.country + " - " + item.city}</p>
                 ) : (
-                  <p className="text-sm font-bold">{item.ciudad || item.alcance}</p>
+                  <p className="text-sm font-bold">{item.pais + " - " + item.ciudad || item.alcance}</p>
                 )}
                 <span className="mt-1 mb-6 text-sm rounded truncated-text">
                   {type === "ficha" ? item.need : item.descripcionIniciativa}
