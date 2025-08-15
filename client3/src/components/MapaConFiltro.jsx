74ed732b52e1c8e7c987609ef2215cb857b88148
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-08-14 20:26:00 -0400

    13 agosto

--- client3/src/components/MapaConFiltro.jsx
+++ client3/src/components/MapaConFiltro.jsx
@@ -58,7 +58,7 @@ const MapaConFiltro = () => {
   }, []);
 
   return (
-    <div className="flex flex-col md:flex-row items-center justify-between bg-white p-8 rounded-xl shadow-md mx-full">
+    <div className="flex flex-col md:flex-row items-center justify-between bg-white  rounded-xl shadow-md mx-full">
       <div className="relative w-full h-150">
         {iniciativas.length > 0 && (
           <Map
