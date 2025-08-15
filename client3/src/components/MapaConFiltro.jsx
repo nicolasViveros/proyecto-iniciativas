5536900b1b72198c0250d654b62205b6d5a16dcf
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-08-14 20:34:57 -0400

    13 agosto

--- client3/src/components/MapaConFiltro.jsx
+++ client3/src/components/MapaConFiltro.jsx
@@ -58,7 +58,7 @@ const MapaConFiltro = () => {
   }, []);
 
   return (
-    <div className=" bg-white px-2 sm:px-8 rounded-xl shadow-md mx-full">
+    <div className=" bg-white px-2 sm:px-8 rounded-xl shadow-md mx-auto">
       <div className="relative w-full h-150">
         {iniciativas.length > 0 && (
           <Map
