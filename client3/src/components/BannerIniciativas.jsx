900a487af9d93c2148397639bb9a2f78c21adbb0
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-08-19 18:24:52 -0400

    19 agosto

--- client3/src/components/BannerIniciativas.jsx
+++ client3/src/components/BannerIniciativas.jsx
@@ -21,7 +21,7 @@ const BannerIniciativas = () => {
           <button onClick={() => navigate("/nueva-ficha")} className="bg-[#5d5593] text-white px-4 py-3 rounded-xl hover:bg-[#a49fc4]">
             Postular iniciativa
           </button>
-          <button className="border border-[#5d5593] text-[#5d5593] px-4 py-2 rounded-xl hover:bg-[#a49fc4]">
+          <button onClick={() => navigate("/")} className="border border-[#5d5593] text-[#5d5593] px-4 py-2 rounded-xl hover:bg-[#a49fc4]">
             Más información
           </button>
         </div>
