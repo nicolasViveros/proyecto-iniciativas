9e742bd982cbc5a68ead97d50b8df9fc8dfe2d26
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2025-08-20 13:28:08 -0400

    19 agosto

--- client3/src/pages/FichaPage.jsx
+++ client3/src/pages/FichaPage.jsx
@@ -205,7 +205,7 @@ const FichaPage = () => {
               </div>
             ))}
           </div>
-          <p className="border-y border-[#D9D6E1] pb-1">
+          <p className="border-b border-[#D9D6E1] pb-1">
             <strong>Categoría del proyecto:</strong>{" "}
           </p>
           <p>{ficha.category}</p>
